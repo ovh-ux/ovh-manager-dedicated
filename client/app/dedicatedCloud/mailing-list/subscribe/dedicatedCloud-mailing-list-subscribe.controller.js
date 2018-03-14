4b788ce403c63d21e3e1df65bb09a6a293ee375e
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-03-14 15:25:53 +0100

    feat(dedicatedcloud): subscribe to ML modal through ui-router

--- client/app/dedicatedCloud/mailing-list/subscribe/dedicatedCloud-mailing-list-subscribe.controller.js
+++ client/app/dedicatedCloud/mailing-list/subscribe/dedicatedCloud-mailing-list-subscribe.controller.js
@@ -1,21 +1,69 @@
-angular.module("App").controller("DedicatedCloudMailingCtrl", ($scope, dedicatedCloudMailingList, Alerter) => {
-    $scope.load = {
-        loading: false
-    };
+angular.module("App").controller("DedicatedCloudMailingCtrl", class DedicatedCloudMailingCtrl {
 
-    $scope.mailing = {
-        pcc: $scope.currentActionData,
-        email: null
-    };
+    constructor ($uibModalInstance, User, dedicatedCloudMailingList, Alerter, translator) {
+        // dependencies injections
+        this.$uibModalInstance = $uibModalInstance;
+        this.User = User;
+        this.dedicatedCloudMailingList = dedicatedCloudMailingList;
+        this.Alerter = Alerter;
+        this.translator = translator;
 
-    $scope.subscribe = function () {
-        $scope.load.loading = true;
-        dedicatedCloudMailingList.postMailingList($scope.mailing.email, $scope.mailing.pcc)
-            .then(() => Alerter.success($scope.tr("dedicatedCloud_subscribe_mailing_step2_success", $scope.mailing.email), "dedicatedCloud"))
-            .catch((err) => Alerter.alertFromSWS($scope.tr("dedicatedCloud_subscribe_mailing_step2_error", $scope.mailing.email), err, "dedicatedCloud"))
-            .finally(() => {
-                $scope.resetAction();
-                $scope.load.loading = false;
-            });
-    };
+        // controller attributes
+        this.loading = {
+            init: false,
+            subscribe: false
+        };
+
+        this.canSuscribe = true;
+        this.model = {
+            email: null
+        };
+
+        this.subscribeToMailingList = this.onWidzardFinish.bind(this);
+    }
+
+    /* =============================
+    =            EVENTS            =
+    ============================== */
+
+    onWidzardFinish () {
+        this.loading.subscribe = true;
+
+        return this.dedicatedCloudMailingList.postMailingList(this.model.email, "pcc@ml.ovh.net").then(() =>
+            this.Alerter.success(this.translator.tr("dedicatedCloud_subscribe_mailing_step2_success", this.model.email), "dedicatedCloud")
+        ).catch((error) =>
+            this.Alerter.error([this.translator.tr("dedicatedCloud_subscribe_mailing_step2_error", this.model.email), _.get(error, "message")].join(". "), "dedicatedCloud_alert")
+        ).finally(() => {
+            this.$uibModalInstance.close();
+            this.loading.subscribe = false;
+        });
+    }
+
+    /* -----  End of EVENTS  ------ */
+
+    /* =====================================
+    =            INITIALIZATION            =
+    ====================================== */
+
+    $onInit () {
+        this.loading.init = true;
+
+        // need to check if user can subscribe to ML
+        return this.dedicatedCloudMailingList.getAvailableMailingLists().then((mailingLists) => {
+            this.canSubscribe = _.indexOf(mailingLists, "pcc@ml.ovh.net") > -1;
+
+            if (this.canSubscribe) {
+                return this.User.getUser().then((user) => {
+                    this.model.email = user.email;
+                });
+            }
+
+            return this.canSubscribe;
+        }).finally(() => {
+            this.loading.init = false;
+        });
+    }
+
+    /* -----  End of INITIALIZATION  ------ */
+
 });
