7111ee49f02d0f5f66e4d7310e750a2150d723cc
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-19 14:43:32 +0100

    fix(server tasks): replace table with oui-datagrid

--- client/app/dedicated/server/task/dedicated-server-task.js
+++ client/app/dedicated/server/task/dedicated-server-task.js
@@ -1,25 +1,12 @@
-angular.module("App").controller("TaskCtrl", ($scope, $stateParams, Server, TASK_STATUS) => {
-    $scope.tasks = null;
-    $scope.task_status = TASK_STATUS;
+angular.module("App").controller("TaskCtrl", ($scope, $stateParams, $q, Server, Alerter) => {
 
-    $scope.$on("tasks.update", () => {
-        $scope.$broadcast("paginationServerSide.reload", "tasksTable");
+    $scope.loadDatagridTasks = ({ offset, pageSize }) => Server.getTasks($stateParams.productId, pageSize, offset - 1).then((result) => ({
+        data: _.get(result, "list.results"),
+        meta: {
+            totalCount: result.count
+        }
+    })).catch((err) => {
+        Alerter.alertFromSWS($scope.tr("server_configuration_task_loading_error"), err.data, "taskAlert");
+        return $q.reject(err);
     });
-
-    $scope.loadTasks = function (elementsByPage, elementsToSkip) {
-        $scope.loading = true;
-        $scope.error = false;
-
-        Server.getTasks($stateParams.productId, elementsByPage, elementsToSkip).then(
-            (results) => {
-                $scope.tasks = results;
-                $scope.loading = false;
-            },
-            (data) => {
-                $scope.loading = false;
-                $scope.error = true;
-                $scope.setMessage($scope.tr("server_configuration_task_loading_error"), data.data);
-            }
-        );
-    };
 });
