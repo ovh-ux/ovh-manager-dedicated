d8806238ed4d4b4469ddd833b87e5da51e04bc12
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-20 13:45:44 +0100

    fix(pcc datacenters): add missing error rejection

--- client/app/dedicatedCloud/datacenters/dedicatedCloud-datacenters.controller.js
+++ client/app/dedicatedCloud/datacenters/dedicatedCloud-datacenters.controller.js
@@ -1,4 +1,4 @@
-angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($scope, $state, $stateParams, constants, DedicatedCloud) => {
+angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($q, $scope, $state, $stateParams, constants, DedicatedCloud) => {
     "use strict";
 
     $scope.loadDatacenters = ({ offset, pageSize }) => DedicatedCloud.getDatacentersInformations($stateParams.productId, pageSize, offset - 1).then((result) => ({
@@ -12,6 +12,7 @@ angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($scope, $stat
             message: err.message,
             type: "ERROR"
         });
+        return $q.reject(err);
     }).finally(() => {
         $scope.loading = false;
     });
