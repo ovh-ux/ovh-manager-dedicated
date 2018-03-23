631bc21e2cb5bf8f6b48f00878c46fc460802c1f
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-23 14:07:14 +0100

    fix(pcc order): fix button href

--- client/app/dedicatedCloud/datacenters/dedicatedCloud-datacenters.controller.js
+++ client/app/dedicatedCloud/datacenters/dedicatedCloud-datacenters.controller.js
@@ -1,7 +1,8 @@
-angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($scope, $stateParams, DedicatedCloud) => {
+angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($scope, $state, $stateParams, constants, DedicatedCloud) => {
     "use strict";
 
     $scope.datacenters = null;
+    $scope.constants = constants;
 
     $scope.loadDatacenters = function (elementsByPage, elementsToSkip) {
         $scope.loading = true;
@@ -31,5 +32,25 @@ angular.module("App").controller("DedicatedCloudDatacentersCtrl", ($scope, $stat
             $scope.discount.AMDPCC = true;
         }
         return hasDiscount;
+    };
+
+    $scope.orderDatastore = (datacenter) => {
+        if (constants.target === "US") {
+            $state.go("app.dedicatedClouds.datacenter.datastores.orderUS", {
+                datacenterId: datacenter.id
+            });
+        } else {
+            $scope.setAction("datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order", datacenter, true);
+        }
+    };
+
+    $scope.orderHost = (datacenter) => {
+        if (constants.target === "US") {
+            $state.go("app.dedicatedClouds.datacenter.hosts.orderUS", {
+                datacenterId: datacenter.id
+            });
+        } else {
+            $scope.setAction("datacenter/host/order/dedicatedCloud-datacenter-host-order", datacenter, true);
+        }
     };
 });
