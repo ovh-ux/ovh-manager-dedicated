eac1205505d2c28bf839701cbe140e88a0bc5465
Thomas SOËTE <thomas.soete@corp.ovh.com>
2018-03-30 14:34:42 +0000

    feat: handle roles in user tab

--- client/app/account/user/security/users/update/user-users-update.controller.js
+++ client/app/account/user/security/users/update/user-users-update.controller.js
@@ -1,17 +1,22 @@
 "use strict";
 angular.module("UserAccount.controllers").controller("UserAccountUsersUpdateCtrl", class UserAccountUsersUpdateCtrl {
 
-    constructor ($scope, UseraccountUsersService, Alerter, $translate) {
+    constructor ($scope, User, UseraccountUsersService, Alerter, $translate) {
         this.$scope = $scope;
+        this.userService = User;
         this.usersService = UseraccountUsersService;
         this.alerter = Alerter;
         this.$translate = $translate;
+        this.me = null;
         this.user = angular.copy($scope.currentActionData);
         this.loader = false;
         this.DESCRIPTION_MAX_LENGTH = 255;
     }
 
     $onInit () {
+        this.userService.getUser().then((data) => {
+            this.me = data;
+        });
         this.$scope.updateUser = this.updateUser.bind(this);
     }
 
