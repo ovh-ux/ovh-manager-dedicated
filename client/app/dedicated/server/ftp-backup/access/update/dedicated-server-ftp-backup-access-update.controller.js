f1d32f05b74d3af13d3e7a3769db33850abb66a5
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-05-03 14:11:51 +0200

    fix(): remove old tr method

--- client/app/dedicated/server/ftp-backup/access/update/dedicated-server-ftp-backup-access-update.controller.js
+++ client/app/dedicated/server/ftp-backup/access/update/dedicated-server-ftp-backup-access-update.controller.js
@@ -1,9 +1,10 @@
 angular.module("App")
     .controller("DedicatedServerFtpBackupAccessUpdateController", class DedicatedServerFtpBackupAccessUpdateController {
-        constructor ($rootScope, $scope, $stateParams, Alerter, Server) {
+        constructor ($rootScope, $scope, $stateParams, $translate, Alerter, Server) {
             this.$rootScope = $rootScope;
             this.$scope = $scope;
             this.$stateParams = $stateParams;
+            this.$translate = $translate;
             this.Alerter = Alerter;
             this.Server = Server;
         }
@@ -24,9 +25,9 @@ angular.module("App")
             return this.Server.putFtpBackupIp(this.$stateParams.productId, this.access.ipBlock, this.form.access.ftp, this.form.access.nfs, this.form.access.cifs)
                 .then(() => {
                     this.$rootScope.$broadcast("server.ftpBackup.access.reload");
-                    this.Alerter.success(this.$scope.tr("server_configuration_ftpbackup_set_success", this.access.ipBlock), "server_tab_ftpbackup_alert");
+                    this.Alerter.success(this.$translate.instant("server_configuration_ftpbackup_set_success", { t0: this.access.ipBlock }), "server_tab_ftpbackup_alert");
                 })
-                .catch((err) => this.Alerter.alertFromSWS(this.$scope.tr("server_configuration_ftpbackup_set_fail", this.access.ipBlock), err, "server_tab_ftpbackup_alert"))
+                .catch((err) => this.Alerter.alertFromSWS(this.$translate.instant("server_configuration_ftpbackup_set_fail", { t0: this.access.ipBlock }), err, "server_tab_ftpbackup_alert"))
                 .finally(() => {
                     this.isUpdating = false;
                     this.$scope.resetAction();
