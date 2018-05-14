b8c06a68592eb93ca80bcb60ed5349bf268665ed
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-05-14 13:33:27 +0200

    refactor(cdn dedicated ssl): refactor delete ssl modal

--- client/app/cdn/dedicated/manage/ssl/delete/cdn-dedicated-ssl-delete.controller.js
+++ client/app/cdn/dedicated/manage/ssl/delete/cdn-dedicated-ssl-delete.controller.js
@@ -1,17 +1,68 @@
-angular.module("App").controller("CdnDeleteSslCtrl", ($scope, $stateParams, $translate, Cdn) => {
-    "use strict";
+angular.module("App").controller("CdnDeleteSslCtrl", class CdnDeleteSslCtrl {
 
-    $scope.ssl = $scope.currentActionData;
+    constructor ($state, $stateParams, $translate, OvhApiCdn, Alerter) {
+        // dependencies injections
+        this.$state = $state;
+        this.$stateParams = $stateParams;
+        this.$translate = $translate;
+        this.OvhApiCdn = OvhApiCdn;
+        this.Alerter = Alerter;
 
-    $scope.deleteSsl = function () {
-        $scope.resetAction();
-        Cdn.deleteSsl($stateParams.productId).then(
-            () => {
-                $scope.setMessage($translate.instant("cdn_configuration_delete_ssl_success"), true);
-            },
-            (data) => {
-                $scope.setMessage($translate.instant("cdn_configuration_delete_ssl_fail", { t0: $scope.ssl.name }), data);
+        // controller attributes
+        this.loading = {
+            init: false,
+            "delete": false
+        };
+
+        this.ssl = null;
+    }
+
+    /* =============================
+    =            EVENTS            =
+    ============================== */
+
+    onPrimaryActionBtnClick () {
+        this.loading.delete = true;
+
+        return this.OvhApiCdn.Dedicated().Ssl().v6().remove({
+            serviceName: this.$stateParams.productId
+        }, this.model).$promise.then(() => {
+            this.Alerter.success(this.$translate.instant("cdn_dedicated_ssl_delete_success"), "cdnDedicatedManage");
+        }).catch((error) => {
+            this.Alerter.error([this.$translate.instant("cdn_dedicated_ssl_delete_error", {
+                t0: this.ssl.name
+            }), _.get(error, "data.message")].join(" "), "cdnDedicatedManage");
+        }).finally(() => {
+            this.loading.delete = false;
+            this.$state.go("^");
+        });
+    }
+
+    /* -----  End of EVENTS  ------ */
+
+    /* =====================================
+    =            INITIALIZATION            =
+    ====================================== */
+
+    $onInit () {
+        this.loading.init = true;
+
+        return this.OvhApiCdn.Dedicated().Ssl().v6().get({
+            serviceName: this.$stateParams.productId
+        }).$promise.then((ssl) => {
+            this.ssl = ssl;
+        }).catch((error) => {
+            if (_.get(error, "status") === 404) {
+                return null;
             }
-        );
-    };
+
+            return this.Alerter.error([this.$translate.instant("cdn_dedicated_ssl_add_load_error"), _.get(error, "data.message")].join(" "), "cdnDedicatedManage");
+        }).finally(() => {
+            this.loading.init = false;
+        });
+    }
+
+    /* -----  End of INITIALIZATION  ------ */
+
+
 });
