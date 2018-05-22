c10d4cf0017d90d96c4baf7a39511956cee4d2b5
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2018-05-22 16:31:27 +0200

    fix(): license header actions

---
+++ client/app/license/spla/add/licence-spla-add.routes.js
@@ -0,0 +1,9 @@
+angular.module("Module.license").config(($stateProvider) => {
+
+    $stateProvider.state("app.license.dashboard.spla-add", {
+        url: "/spla/add",
+        templateUrl: "license/spla/add/license-spla-add.html",
+        controller: "LicenseSplaAddCtrl",
+        layout: "modal"
+    });
+});
