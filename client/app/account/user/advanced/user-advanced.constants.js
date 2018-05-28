18f1b8195a47eb744f11832cd3b75eafa419b576
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-05-28 16:36:25 +0200

    refactor(account user advanced): rewrite controller as class
    ref: #328, #359

---
+++ client/app/account/user/advanced/user-advanced.constants.js
@@ -0,0 +1,4 @@
+angular
+    .module("UserAccount")
+    .constant("GITHUB_ORGANIZATION_URL", "https://github.com/ovh-ux")
+    .constant("GITTER_URL", "https://gitter.im/ovh/ux");
--- client/app/account/user/advanced/user-advanced.controller.js
+++ client/app/account/user/advanced/user-advanced.controller.js
@@ -1,41 +1,45 @@
-angular.module("UserAccount").controller("UserAccountAdvancedController", [
-    "UserAccount.services.Infos",
-    "Alerter",
-    "$translate",
-    function (userAccountServiceInfos, Alerter, $translate) {
-        "use strict";
+angular
+    .module("UserAccount")
+    .controller("UserAccountAdvancedController", class UserAccountAdvancedController {
+        constructor ($translate, Alerter, OvhApiMe, GITHUB_ORGANIZATION_URL, GITTER_URL) {
+            this.$translate = $translate;
+            this.Alerter = Alerter;
+            this.OvhApiMe = OvhApiMe;
+            this.GITHUB_ORGANIZATION_URL = GITHUB_ORGANIZATION_URL;
+            this.GITTER_URL = GITTER_URL;
+        }
 
-        this.isLoadingDeveloperMode = false;
+        $onInit () {
+            this.api = {
+                me: {
+                    accessRestriction: this.OvhApiMe.AccessRestriction().v6()
+                }
+            };
+            this.isLoading = true;
 
-        this.$ngInit = () => {
-            this.isLoadingDeveloperMode = true;
-            userAccountServiceInfos
-                .getDeveloperMode()
-                .then((developmentMode) => {
-                    this.developmentMode = developmentMode;
+            return this.api.me.accessRestriction
+                .developerMode().$promise
+                .then((developerModeRestriction) => {
+                    this.developerModeRestriction = developerModeRestriction;
+                    return developerModeRestriction;
                 })
                 .finally(() => {
-                    this.isLoadingDeveloperMode = false;
+                    this.isLoading = false;
                 });
-        };
+        }
 
-        this.updateDevelopmentMode = () => {
-            this.isLoadingDeveloperMode = true;
-            const successKey = this.developmentMode.enabled ? "user_account_advanced_section_developer_alert_success_enabled" : "user_account_advanced_section_developer_alert_success_disabled";
-            userAccountServiceInfos
-                .updateDeveloperMode(this.developmentMode)
+        updateDeveloperModeRestriction () {
+            this.isUpdating = true;
+
+            return this.api.me.accessRestriction
+                .updateDeveloperMode(this.developerModeRestriction).$promise
                 .then(() => {
-                    const zone = "useraccount.alerts.dashboardAdvanced";
-                    return Alerter.success($translate.instant(successKey), zone);
-                }, () => {
-                    const zone = "useraccount.alerts.dashboardAdvanced";
-                    return Alerter.error($translate.instant("user_account_advanced_section_developer_alert_error"), zone);
+                    const successKey = `user_account_advanced_section_developer_alert_success_${this.developerModeRestriction.enabled ? "enabled" : "disabled"}`;
+                    return this.Alerter.success(this.$translate.instant(successKey), "useraccount.alerts.dashboardAdvanced");
                 })
+                .catch(() => this.Alerter.error(this.$translate.instant("user_account_advanced_section_developer_alert_error"), "useraccount.alerts.dashboardAdvanced"))
                 .finally(() => {
-                    this.isLoadingDeveloperMode = false;
+                    this.isUpdating = false;
                 });
-        };
-
-        this.$ngInit();
-    }
-]);
+        }
+    });
