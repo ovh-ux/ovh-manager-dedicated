31f035b122742e0806658303215a71a651b5cdf7
Florian Renaut <florian.renaut@corp.ovh.com>
2018-05-30 14:20:34 +0200

    fix(pcc security): replace table with datagrid

--- client/app/dedicatedCloud/security/delete/dedicatedCloud-security-delete.controller.js
+++ client/app/dedicatedCloud/security/delete/dedicatedCloud-security-delete.controller.js
@@ -1,24 +1,22 @@
 angular.module("App").controller("DedicatedCloudSecurityPolicyDeleteCtrl", ($scope, $stateParams, DedicatedCloud, $translate, Alerter) => {
-    const policies = $scope.currentActionData.policies;
 
     $scope.entriesToDelete = $scope.currentActionData.selectedPolicies;
 
     $scope.deleteEntries = function () {
         $scope.resetAction();
-        DedicatedCloud[$scope.entriesToDelete.length > 1 ? "deleteSecurityPolicies" : "deleteSecurityPolicy"]($stateParams.productId, $scope.entriesToDelete)
-            .then(() => Alerter.success(
-                $scope.entriesToDelete.length > 1 ?
-                    $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_success_other") :
-                    $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_success_one")
-                , "dedicatedCloud"))
-            .catch((err) => Alerter.alertFromSWS(
-                $scope.entriesToDelete.length > 1 ?
-                    $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_fail_other") :
-                    $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_fail_one")
-                , err, "dedicatedCloud"));
-    };
-
-    $scope.getPolicyIP = function (id) {
-        return _.find(policies, { id }).network;
+        DedicatedCloud[$scope.entriesToDelete.length > 1 ? "deleteSecurityPolicies" : "deleteSecurityPolicy"](
+            $stateParams.productId,
+            _.pluck($scope.entriesToDelete, "id")
+        ).then(() => Alerter.success(
+            $scope.entriesToDelete.length > 1 ?
+                $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_success_other") :
+                $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_success_one")
+            , "dedicatedCloud")
+        ).catch((err) => Alerter.alertFromSWS(
+            $scope.entriesToDelete.length > 1 ?
+                $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_fail_other") :
+                $translate.instant("dedicatedCloud_configuration_SECURITY_policy_delete_fail_one")
+            , err, "dedicatedCloud")
+        );
     };
 });
