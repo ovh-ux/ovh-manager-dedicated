d571870def76582cb3b5c6255a5f6d51ba07acad
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-05-31 10:01:26 +0200

    refactor(cdn domain): use resolve in statistics view

--- client/app/cdn/dedicated/domain/statistics/cdn-dedicated-domain-statistics.controller.js
+++ client/app/cdn/dedicated/domain/statistics/cdn-dedicated-domain-statistics.controller.js
@@ -1,50 +1,68 @@
-angular.module("App").controller("CdnDomainStatisticsCtrl", ($scope, $stateParams, $translate, Cdn, CdnDomain) => {
-    $scope.model = null;
-    $scope.consts = null;
-    $scope.loadingStats = false;
-    $scope.loadingConsts = false;
+angular.module("App").controller("CdnDomainStatisticsCtrl", class CdnDomainStatisticsCtrl {
 
-    function createChart (data) {
-        $scope.series = [];
-        $scope.data = [];
+    constructor ($stateParams, $translate, OvhApiCdnDedicated, Alerter, cdnDomain) {
+        // injections
+        this.$stateParams = $stateParams;
+        this.$translate = $translate;
+        this.OvhApiCdnDedicated = OvhApiCdnDedicated;
+        this.Alerter = Alerter;
+        this.cdnDomain = cdnDomain;
 
-        $scope.labels = _.map(_.get(data, "cdn.values"), (value, index) => {
-            const source = data.backend || data.cdn;
+        // other attributes used in view
+        this.availablePeriods = ["day", "week", "month"];
+        this.availableValues = ["bandwidth", "request"];
+
+        this.loading = {
+            stats: false
+        };
+
+        this.model = {
+            period: _.first(this.availablePeriods),
+            value: _.first(this.availableValues)
+        };
+    }
+
+    createChart (stats) {
+        this.series = [];
+        this.data = [];
+
+        this.labels = _.map(_.get(stats, "cdn.values"), (value, index) => {
+            const source = stats.backend || stats.cdn;
             const start = _.get(source, "pointStart");
             const interval = _.get(source, "pointInterval.standardSeconds");
             return moment(start).add((index + 1) * interval, "seconds").calendar();
         });
-        $scope.series.push($translate.instant(`cdn_stats_legend_${$scope.model.dataType.toLowerCase()}_cdn`));
-        $scope.series.push($translate.instant(`cdn_stats_legend_${$scope.model.dataType.toLowerCase()}_backend`));
-        $scope.data.push(_.map(_.get(data, "cdn.values"), (value) => value.y));
-        $scope.data.push(_.map(_.get(data, "backend.values"), (value) => value.y));
+        this.series.push(this.$translate.instant(`cdn_stats_legend_${this.model.value}_cdn`));
+        this.series.push(this.$translate.instant(`cdn_stats_legend_${this.model.value}_backend`));
+        this.data.push(_.map(_.get(stats, "cdn.values"), (value) => value.y));
+        this.data.push(_.map(_.get(stats, "backend.values"), (value) => value.y));
     }
 
-    $scope.getStatistics = () => {
-        $scope.loadingStats = true;
-        return Cdn.getStatistics($stateParams.productId, $scope.model)
-            .then((data) => createChart(data))
-            .finally(() => {
-                $scope.loadingStats = false;
-            });
-    };
+    getStatistics () {
+        this.loading.stats = true;
 
-    function init () {
-        $scope.loadingConsts = true;
-        CdnDomain.getSelected($stateParams.productId, $stateParams.domain)
-            .then((domain) => {
-                $scope.model = {
-                    domain: domain.domain
-                };
-                Cdn.getStatisticsConsts(domain).then((data) => {
-                    $scope.consts = data;
-                    $scope.model.dataType = data.defaultType;
-                    $scope.model.period = data.defaultPeriod;
-                    $scope.loadingConsts = false;
-                    $scope.getStatistics();
-                });
-            });
+        return this.OvhApiCdnDedicated.v6().swsGetStatistics({
+            serviceName: this.$stateParams.productId,
+            domain: this.$stateParams.domain,
+            period: this.model.period.toUpperCase(),
+            dataType: this.model.value.toUpperCase()
+        }).$promise.then((stats) => {
+            this.createChart(stats);
+        }).catch((error) => {
+            this.Alerter.error([this.$translate.instant("cdn_dedicated_statistics_load_error"), _.get(error, "data.message")].join(" "), "cdnDedicatedDomain");
+        }).finally(() => {
+            this.loading.stats = false;
+        });
     }
 
-    init();
+    /* =====================================
+    =            INITIALIZATION            =
+    ====================================== */
+
+    $onInit () {
+        this.getStatistics();
+    }
+
+    /* -----  End of INITIALIZATION  ------ */
+
 });
