ef4686af59eac0eee9661cf3f4dd1f2b0e4c7779
Kevin Manson <kevin.manson@corp.ovh.com>
2018-06-05 16:28:21 -0400

    feat(billing): add confirm terminate for all subsidiaries

--- client/app/account/billing/confirmTerminate/billing-confirmTerminate.controller.js
+++ client/app/account/billing/confirmTerminate/billing-confirmTerminate.controller.js
@@ -1,57 +1,81 @@
-angular.module("Billing.controllers").controller("Billing.controllers.TerminateServiceCtrl", function ($stateParams, BillingTerminate) {
-    "use strict";
+angular.module("Billing.controllers")
+    .controller("Billing.controllers.TerminateServiceCtrl", function ($q, $stateParams, BillingTerminate, User) {
+        "use strict";
 
-    this.reasons = ["LACK_OF_PERFORMANCES", "TOO_EXPENSIVE", "TOO_HARD_TO_USE", "NOT_RELIABLE", "NOT_NEEDED_ANYMORE", "MIGRATED_TO_COMPETITOR", "MIGRATED_TO_ANOTHER_OVH_PRODUCT", "OTHER"];
+        this.reasons = [
+            "LACK_OF_PERFORMANCES",
+            "TOO_EXPENSIVE",
+            "TOO_HARD_TO_USE",
+            "NOT_RELIABLE",
+            "NOT_NEEDED_ANYMORE",
+            "MIGRATED_TO_COMPETITOR",
+            "MIGRATED_TO_ANOTHER_OVH_PRODUCT",
+            "OTHER"
+        ];
 
-    this.serviceId = $stateParams.id;
-    this.serviceState = null;
-    this.token = $stateParams.token;
-    this.loading = true;
-    this.terminating = false;
-    this.error = false;
-    this.globalError = null;
-
-    this.init = function () {
-        if (!this.token || !this.serviceId) {
-            this.globalError = true;
-            return;
-        }
-
+        this.serviceId = $stateParams.id;
+        this.serviceState = null;
+        this.token = $stateParams.token;
         this.loading = true;
-        BillingTerminate
-            .getServiceInfo(this.serviceId)
-            .then((serviceInfos) => {
-                this.serviceInfos = serviceInfos;
-                return serviceInfos.serviceId;
-            })
-            .then((serviceId) =>
-                BillingTerminate.getServiceApi(serviceId, true).then((service) => {
-                    this.serviceState = _.get(service, "resource.state");
-                })
-            )
-            .catch(() => {
+        this.userLoading = true;
+        this.terminating = false;
+        this.error = false;
+        this.globalError = null;
+
+        this.init = function () {
+            if (!this.token || !this.serviceId) {
                 this.globalError = true;
-            })
-            .finally(() => {
-                this.loading = false;
-            });
-    };
+                return;
+            }
 
-    this.confirmTermination = function () {
-        this.terminating = true;
-        BillingTerminate
-            .confirmTermination(this.serviceId, this.serviceInfos.domain, this.reason, this.commentary, this.token)
-            .then(() => {
-                this.error = false;
-                this.serviceState = "suspending";
-            })
-            .catch(() => {
-                this.error = true;
-            })
-            .finally(() => {
-                this.terminating = false;
+            $q.all([
+                this.loadUser(),
+                this.loadService()
+            ]).catch(() => {
+                this.globalError = true;
             });
-    };
+        };
 
-    this.init();
-});
+        this.loadUser = function () {
+            return User.getUser()
+                .then((user) => {
+                    this.USVersion = user && user.ovhSubsidiary === "US";
+                })
+                .finally(() => {
+                    this.userLoading = false;
+                });
+        };
+
+        this.loadService = function () {
+            return BillingTerminate
+                .getServiceInfo(this.serviceId)
+                .then((serviceInfos) => {
+                    this.serviceInfos = serviceInfos;
+                    return serviceInfos.serviceId;
+                })
+                .then((serviceId) => BillingTerminate.getServiceApi(serviceId, true).then((service) => {
+                    this.serviceState = _.get(service, "resource.state");
+                }))
+                .finally(() => {
+                    this.loading = false;
+                });
+        };
+
+        this.confirmTermination = function () {
+            this.terminating = true;
+            BillingTerminate
+                .confirmTermination(this.serviceId, this.serviceInfos.domain, this.reason, this.commentary, this.token)
+                .then(() => {
+                    this.error = false;
+                    this.serviceState = "suspending";
+                })
+                .catch(() => {
+                    this.error = true;
+                })
+                .finally(() => {
+                    this.terminating = false;
+                });
+        };
+
+        this.init();
+    });
