b74e0a3904da0c1b87245249bab6ab01e53bf41d
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-06-06 10:44:09 +0200

    refactor(cdn domain): create delete modal state

--- client/app/cdn/dedicated/domain/delete/cdn-dedicated-domain-delete.controller.js
+++ client/app/cdn/dedicated/domain/delete/cdn-dedicated-domain-delete.controller.js
@@ -1,16 +1,48 @@
-angular.module("App").controller("CdnDeleteDomainCtrl", ($scope, $stateParams, $translate, CdnDomain) => {
+angular.module("App").controller("CdnDomainDeleteCtrl", class CdnDomainDeleteCtrl {
 
-    $scope.domain = $stateParams.domain;
+    constructor ($state, $stateParams, $translate, cdnDomain, ouiMessageAlerter, OvhApiCdnDedicated) {
+        // Injections
+        this.$state = $state;
+        this.$stateParams = $stateParams;
+        this.$translate = $translate;
+        this.cdnDomain = cdnDomain;
+        this.ouiMessageAlerter = ouiMessageAlerter;
+        this.OvhApiCdnDedicated = OvhApiCdnDedicated;
 
-    $scope.deleteEntry = function () {
-        $scope.resetAction();
-        CdnDomain.deleteDomain($stateParams.productId, $stateParams.domain).then(
-            () => {
-                $scope.setMessage($translate.instant("cdn_configuration_delete_domain_success"), true);
-            },
-            (data) => {
-                $scope.setMessage($translate.instant("cdn_configuration_delete_domain_fail"), angular.extend(data, { type: "ERROR" }));
-            }
-        );
-    };
+        // Other attributes used in view
+        this.loading = {
+            remove: false
+        };
+    }
+
+    /* =============================
+    =            EVENTS            =
+    ============================== */
+
+    onCdnDomainDeleteFormSubmit () {
+        this.loading.remove = true;
+
+        return this.OvhApiCdnDedicated.Domains().v6().remove({
+            serviceName: this.$stateParams.productId,
+            domain: this.$stateParams.domain
+        }).$promise.then(() => {
+            this.ouiMessageAlerter.success(this.$translate.instant("cdn_dedicated_domain_delete_success"), "app.networks.cdn.dedicated.domain");
+
+            this.$state.go("^", {}, {
+                reload: true
+            });
+        }).catch((error) => {
+            this.ouiMessageAlerter.error([this.$translate.instant("cdn_dedicated_domain_delete_fail", {
+                t0: this.cdnDomain.domain
+            }), _.get(error, "data.message")].join(" "), "app.networks.cdn.dedicated.domain");
+
+            this.$state.go("^");
+        }).finally(() => {
+            this.loading.remove = false;
+        });
+    }
+
+    /* -----  End of EVENTS  ------ */
+
+
 });
