41e4d507e0d16ca0fd597c24f46b065fc33eabcd
Florian Renaut <florian.renaut@corp.ovh.com>
2018-06-14 21:10:38 +0200

    fix(pcc): replace table with datagrid in datastore section (#412)

--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
@@ -1,51 +1,49 @@
-angular.module("App").controller("DedicatedCloudSubDatacentersDatastoreCtrl", ($scope, $state, $stateParams, $translate, DedicatedCloud, constants) => {
+angular.module("App").controller("DedicatedCloudSubDatacentersDatastoreCtrl", function ($q, $scope, $state, $stateParams, constants, DedicatedCloud) {
     "use strict";
 
-    $scope.datastores = null;
-    $scope.loading = false;
     $scope.constants = constants;
-
     const noConsumptionResponse = new RegExp("no consumption", "i");
 
-    $scope.loadDatastores = function (elementsByPage, elementsToSkip) {
-        $scope.loading = true;
-        DedicatedCloud.getDatastores($stateParams.productId, $stateParams.datacenterId, elementsByPage, elementsToSkip).then(
-            (datastores) => {
-                $scope.datastores = datastores;
-                _.each(datastores.list.results, setHourlyUsage);
-                $scope.loading = false;
-            },
-            (data) => {
-                $scope.loading = false;
-                $scope.setMessage($translate.instant("dedicatedCloud_tab_datastores_loading_error"), angular.extend(data, { type: "ERROR" }));
+    this.loadDatastores = ({ offset, pageSize }) => DedicatedCloud.getDatastores(
+        $stateParams.productId,
+        $stateParams.datacenterId,
+        pageSize,
+        offset - 1
+    ).then((result) => {
+        const datastores = _.get(result, "list.results");
+        return $q.all(datastores.map((dc) => {
+            if (dc.billing === "HOURLY") {
+                return DedicatedCloud.getDatastoreHourlyConsumption(
+                    $stateParams.productId,
+                    $stateParams.datacenterId,
+                    dc.id
+                ).then((response) => {
+                    dc.consumption = _.get(response, "consumption.value");
+                    dc.consumptionLastUpdate = response.lastUpdate;
+                    return dc;
+                }).catch((err) => {
+                    if (noConsumptionResponse.test(err.message)) {
+                        dc.consumption = 0;
+                    } else {
+                        dc.consumption = null;
+                    }
+                    return dc;
+                });
             }
-        );
-    };
+            return $q.when(dc);
+        })).then((data) => ({
+            data,
+            meta: {
+                totalCount: result.count
+            }
+        }));
+    });
 
-    $scope.orderDatastore = (datacenter) => {
+    this.orderDatastore = (datacenter) => {
         if (constants.target === "US") {
             $state.go("app.dedicatedClouds.datacenter.datastores.orderUS");
         } else {
             $scope.setAction("datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order", datacenter.model, true);
         }
     };
-
-    function setHourlyUsage (store) {
-        if (store.billing === "HOURLY") {
-            DedicatedCloud.getDatastoreHourlyConsumption($stateParams.productId, $stateParams.datacenterId, store.id)
-                .then((response) => {
-                    store.consumption = response.consumption.value;
-                    store.consumptionLastUpdate = response.lastUpdate;
-                })
-                .catch((err) => {
-                    if (noConsumptionResponse.test(err.message)) {
-                        store.consumption = 0;
-                    } else {
-                        store.consumption = NaN;
-                    }
-                });
-        } else {
-            store.consumption = "";
-        }
-    }
 });
