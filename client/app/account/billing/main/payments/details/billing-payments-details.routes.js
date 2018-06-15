36a215ff96f2c4613efd5e212f73b37ebbfeba8e
Florian Renaut <florian.renaut@corp.ovh.com>
2018-06-15 10:33:11 +0200

    refactor(payments): fix routes

---
+++ client/app/account/billing/main/payments/details/billing-payments-details.routes.js
@@ -0,0 +1,10 @@
+angular.module("Billing").config(($stateProvider, BILLING_BASE_URL) => {
+
+    $stateProvider.state("app.account.billing.main.paymentsDetails", {
+        url: "/payments/:id/details",
+        templateUrl: `${BILLING_BASE_URL}/main/payments/details/billing-payments-details.html`,
+        controller: "Billing.controllers.PaymentDetailsCtrl",
+        controllerAs: "ctrl"
+    });
+});
+
