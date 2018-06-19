3b97e3d0124c11673823ca21fa0845553bd3730f
Florian Renaut <florian.renaut@corp.ovh.com>
2018-06-19 14:49:55 +0200

    refactor(payment): fix datagrid, add sort & filters

--- client/app/account/billing/main/payments/billing-payments.controller.js
+++ client/app/account/billing/main/payments/billing-payments.controller.js
@@ -1,52 +1,57 @@
-angular.module("Billing.controllers").controller("Billing.controllers.PaymentsCtrl", function ($filter, $scope, $state, $translate, Alerter, BillingdateRangeSelection, BillingPayments, constants, featureAvailability, OvhApiMe) {
+angular.module("Billing.controllers").controller("Billing.controllers.PaymentsCtrl", function ($filter, $q, $scope, $state, $translate, constants, featureAvailability, OvhApiMe) {
     "use strict";
 
-    this.paymentTypes = {
-        model: 0,
-        values: ["0", "creditCard", "debtAccount", "paypal"]
-    };
+    this.loadPayments = ($config) => {
 
-    this.loaders = {
-        payments: false
-    };
+        let request = OvhApiMe.Deposit().v7().query().sort($config.sort.property, $config.sort.dir > 0 ? "ASC" : "DESC");
 
-    this.onDateRangeChanged = () => {
-        $scope.$broadcast("paginationServerSide.loadPage", "1", "paymentsTable");
-    };
-
-    this.paymentRequests = null;
-    this.paymentRequestsHref = $state.href("app.account.billing.payments.request");
-
-
-    this.payments = [];
-
-    this.loadPayments = ({ offset, pageSize }) => {
-
-        const sort = { field: "date", order: "DESC" };
-        const dateFrom = BillingdateRangeSelection.dateFrom;
-        const dateTo = BillingdateRangeSelection.dateTo;
-        let paymentType = this.paymentTypes.model;
-        if (paymentType === "debtAccount") {
-            paymentType = ["debtAccount", "withdrawal"];
-        }
-
-        return BillingPayments.getPaymentIds({
-            dateFrom,
-            dateTo,
-            sort,
-            paymentType
-        }).then((result) => BillingPayments.getPayments(result, offset + pageSize - 1, offset - 1).then((data) => ({
-            data,
-            count: result.length
-        }))).then(({ count, data }) => {
-            this.payments = data;
-            return {
-                data,
-                meta: {
-                    totalCount: count
-                }
-            };
+        _.filter($config.criteria, "property", "date").forEach((crit) => {
+            switch (crit.operator) {
+            case "is":
+                request = request.addFilter("date", "ge", crit.value);
+                request = request.addFilter("date", "le", moment(crit.value).add(1, "day").format("YYYY-MM-DD"));
+                break;
+            case "isAfter":
+                request = request.addFilter("date", "ge", crit.value);
+                break;
+            case "isBefore":
+                request = request.addFilter("date", "ge", crit.value);
+                break;
+            default:
+                break;
+            }
         });
+
+        _.filter($config.criteria, "property", "amount.value").forEach((crit) => {
+            request = request.addFilter("amount.value", {
+                is: "eq",
+                smaller: "lt",
+                bigger: "gt"
+            }[crit.operator], crit.value);
+        });
+
+        _.filter($config.criteria, "property", "paymentInfo.paymentType").forEach((crit) => {
+            request = request.addFilter("paymentInfo.paymentType", {
+                is: "eq",
+                isNot: "ne"
+            }[crit.operator], crit.value);
+        });
+
+        return $q.all({
+            count: request.clone().execute().$promise,
+            deposit: request.clone().expand()
+                .offset($config.offset - 1)
+                .limit($config.pageSize)
+                .execute().$promise
+        }).then(({ count, deposit }) => {
+            this.payments = _.map(deposit, "value");
+            return { count, deposit };
+        }).then(({ count, deposit }) => ({
+            data: _.map(deposit, "value"),
+            meta: {
+                totalCount: count.length
+            }
+        }));
     };
 
     this.getDatasToExport = () => {
@@ -57,7 +62,6 @@ angular.module("Billing.controllers").controller("Billing.controllers.PaymentsCt
             $translate.instant("payments_table_head_type")
         ];
         const result = [header];
-
         return result.concat(this.payments.map((payment) => [
             payment.depositId,
             $filter("date")(payment.date, "mediumDate"),
@@ -66,19 +70,24 @@ angular.module("Billing.controllers").controller("Billing.controllers.PaymentsCt
         ]));
     };
 
-    this.getTranslatedPaiementType = (payment) => payment.paymentInfo ? $translate.instant(`common_payment_type_${payment.paymentInfo.paymentType}`) : $translate.instant("payments_table_type_not_available");
+    this.getTranslatedPaiementType = (payment) => {
+        return payment.paymentInfo ? $translate.instant(`common_payment_type_${payment.paymentInfo.paymentType}`) : $translate.instant("payments_table_type_not_available");
+    };
 
     this.shouldDisplayDepositsLinks = () => featureAvailability.showPDFAndHTMLDepositLinks();
 
     this.displayActionsCol = () => constants.target !== "US";
 
+    this.depositDetailsHref = ({ depositId }) => $state.href("app.account.billing.main.paymentsDetails", { id: depositId });
+
     this.$onInit = () => {
+        this.payments = [];
         if (constants.target === "US") {
             return OvhApiMe.DepositRequest().v6().query().$promise.then((depositRequests) => {
                 this.paymentRequests = depositRequests;
+                this.paymentRequestsHref = $state.href("app.account.billing.payments.request");
             });
         }
-
-        return null;
+        return $q.when(null);
     };
 });
