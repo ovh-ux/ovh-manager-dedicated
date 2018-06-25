9b1f311eb50a02f3cf478e6e3f4d5f1952d9328b
neolitec aka Keveun <kev.manson@gmail.com>
2018-06-25 00:38:18 -0400

    fix(license): fix upgrade button redirection (#425)
    SCFRCA-1212 - update a windows licence - the redirection goes to the home page

--- client/app/license/redirection/upgrade/license-redirection-upgrade.controller.js
+++ client/app/license/redirection/upgrade/license-redirection-upgrade.controller.js
@@ -1,12 +1,21 @@
-angular.module("Module.license").controller("ConfirmUpgradeRedirectionCtrl", ($location, $scope, $timeout) => {
-    "use strict";
+angular.module("Module.license").controller("LicenseRedirectionUpgradeCtrl", class LicenseRedirectionUpgradeCtrl {
+    constructor ($scope, $state, $timeout) {
+        this.$scope = $scope;
+        this.$state = $state;
+        this.$timeout = $timeout;
+    }
 
-    const licenseId = $scope.currentActionData.licenseId;
+    $onInit () {
+        this.licenseId = this.$scope.currentActionData.licenseId;
+        this.$scope.redirectToUpgrade = this.redirectToUpgrade.bind(this);
+    }
 
-    $scope.redirectToUpgrade = function () {
-        $scope.resetAction();
-        $timeout(() => {
-            $location.path(`/configuration/license/upgrade/${licenseId}`);
+    redirectToUpgrade () {
+        this.$scope.resetAction();
+        this.$timeout(() => {
+            this.$state.go("app.license.upgrade", {
+                licenseId: this.licenseId
+            });
         }, 300);
-    };
+    }
 });
