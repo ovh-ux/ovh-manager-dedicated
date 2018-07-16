b5039f30a8fa02acd68d83daac7de0d6b30c860c
varun shivaprasad <varun257@gmail.com>
2018-07-16 13:06:43 +0530

    fix(cache-rules-loading): infinite loading
    reset action
    add alert message
    
    Issue: MFRWW-504

--- client/app/cdn/dedicated/domain/rule/order/cdn-dedicated-domain-rule-order.controller.js
+++ client/app/cdn/dedicated/domain/rule/order/cdn-dedicated-domain-rule-order.controller.js
@@ -15,7 +15,8 @@ angular.module("App").controller("CacherulesAddCtrl", ($scope, $stateParams, $tr
             cdnDedicatedOrderRule.getCacherulePrices($stateParams.productId).then((prices) => {
                 $scope.prices = prices.results;
             }).catch((error) => {
-                Alerter.alertFromSWS($translate.instant("cdn_configuration_cacherules_upgrade_fail"), error, $scope.alert);                
+                $scope.resetAction();
+                Alerter.alertFromSWS($translate.instant("cdn_configuration_cacherules_upgrade_fail"), error, $scope.alert);
             }).finally(() => {
                 $scope.cacheRulesLoading = false;
             });
