43b4142294e0822548b62946e1d0fe8d19a8943c
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-08-07 11:23:34 +0200

    fix(billing.payasyougo): handle empty route.url and route.path

--- client/app/components/services/services.service.js
+++ client/app/components/services/services.service.js
@@ -1,16 +1,24 @@
 angular.module("App").service("ServicesHelper", class ServicesHelper {
 
-    constructor ($http, constants, SERVICES_TARGET_URLS) {
+    constructor ($http, $q, constants, SERVICES_TARGET_URLS) {
         this.$http = $http;
+        this.$q = $q;
         this.constants = constants;
         this.SERVICES_TARGET_URLS = SERVICES_TARGET_URLS;
     }
 
     getServiceDetails (service) {
-        return this.$http.get(_.get(service, "route.url")).then(({ data }) => data);
+        if (_.has(service, "route.url")) {
+            return this.$http.get(_.get(service, "route.url")).then(({ data }) => data);
+        }
+        return this.$q.when({});
     }
 
     getServiceManageUrl (service) {
+        if (!_.has(service, "route.path")) {
+            return "";
+        }
+
         const target = _.get(this.SERVICES_TARGET_URLS, _.get(service, "route.path"));
         const basePath = _.get(this.constants.MANAGER_URLS, target.univers);
 
