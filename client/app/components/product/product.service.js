7cd03cda24294bcc224cda7c3bb0d23078cf53f2
Frederic Espiau <frederic.espiau@corp.ovh.com>
2018-11-09 14:50:22 +0100

    refactor(product): follow eslint rules

--- client/app/components/product/product.service.js
+++ client/app/components/product/product.service.js
@@ -1,124 +1,112 @@
-angular.module('services').service('Products', function ($rootScope, $http, $q, $stateParams, constants) {
-  let products = null;
-  let productsByType = null;
-  const requests = {
-    productsList: null,
-  };
+angular
+  .module('services')
+  .service('Products', function productService($rootScope, $http, $q, $stateParams, constants) {
+    let products = null;
+    let productsByType = null;
+    const requests = {
+      productsList: null,
+    };
 
-  $rootScope.$on('global_display_name_change', (evt, params) => {
-    const result = _.find(products, { name: params.serviceName });
-    result.displayName = angular.copy(params.displayName);
-  });
+    $rootScope.$on('global_display_name_change', (evt, params) => {
+      const result = _.find(products, { name: params.serviceName });
+      result.displayName = angular.copy(params.displayName);
+    });
 
-  function resetCache() {
-    products = null;
-    productsByType = null;
-    requests.productsList = null;
-  }
+    function resetCache() {
+      products = null;
+      productsByType = null;
+      requests.productsList = null;
+    }
 
-  /*
+    /*
         * get product by SWS
         */
-  this.getProducts = function (forceRefresh) {
-    if (forceRefresh === true) {
-      resetCache();
-    }
+    this.getProducts = function getProducts(forceRefresh) {
+      if (forceRefresh === true) {
+        resetCache();
+      }
 
-    return $q
-      .when(products)
-      .then(() => {
-        if (!products) {
-          if (requests.productsList === null) {
-            requests.productsList = $http
-              .get('/sws/products', {
-                serviceType: 'aapi',
-                params: {
-                  universe: 'DEDICATED',
-                  worldPart: constants.target,
-                },
-              })
-              .then((result) => {
-                if (result.status < 300) {
-                  productsByType = result.data;
+      return $q
+        .when(products)
+        .then(() => {
+          if (!products) {
+            if (requests.productsList === null) {
+              requests.productsList = $http
+                .get('/sws/products', {
+                  serviceType: 'aapi',
+                  params: {
+                    universe: 'DEDICATED',
+                    worldPart: constants.target,
+                  },
+                })
+                .then((result) => {
+                  if (result.status < 300) {
+                    productsByType = result.data;
 
-                  if (!products) {
-                    products = [];
+                    if (!products) {
+                      products = [];
+                    }
+
+                    ['dedicatedServers', 'dedicatedClouds', 'vps', 'exchanges', 'networks'].forEach((type) => {
+                      products = products.concat(productsByType[type]);
+                    });
+                    return products;
                   }
+                  return $q.reject(result);
+                });
+            }
 
-                  ['dedicatedServers', 'dedicatedClouds', 'vps', 'exchanges', 'networks'].forEach((type) => {
-                    products = products.concat(productsByType[type]);
-                  });
-                  return products;
-                }
-                return $q.reject(result);
-              });
+            return requests.productsList;
           }
+          return products;
+        })
+        .then(() => products, reason => $q.reject(reason));
+    };
 
-          return requests.productsList;
-        }
-        return products;
-      })
-      .then(() => products, reason => $q.reject(reason));
-  };
-
-  /*
+    /*
         * Get list of products orderBy Type
         */
-  this.getProductsByType = function () {
-    return this.getProducts().then(() => productsByType);
-  };
+    this.getProductsByType = function getProductsByType() {
+      return this.getProducts().then(() => productsByType);
+    };
 
-  /*
+    /*
         * Get the selected product
         */
-  this.getSelectedProduct = function (productId, forceRefresh) {
-    if (!_.isString(productId)) {
-      if (_.isBoolean(productId)) {
-        return this.getSelectedProductFromWeb(productId);
+    this.getSelectedProduct = function getSelectedProduct(productId, forceRefresh) {
+      if (!_.isString(productId)) {
+        if (_.isBoolean(productId)) {
+          return this.getSelectedProductFromWeb(productId);
+        }
+        return this.getSelectedProductFromWeb();
       }
-      return this.getSelectedProductFromWeb();
-    }
 
-    return this
-      .getProducts(forceRefresh)
-      .then(productsList => _.find(productsList, { name: productId }));
-  };
+      return this
+        .getProducts(forceRefresh)
+        .then(productsList => _.find(productsList, { name: productId }));
+    };
 
-  this.getSelectedProductFromWeb = function (forceRefresh) {
-    return this.getProducts(forceRefresh).then((productsList) => {
-      const name = $stateParams.productId ? $stateParams.productId : '';
-      const type = $rootScope.currentSectionInformation
-        ? $rootScope.currentSectionInformation.toUpperCase()
-        : null;
+    this.getSelectedProductFromWeb = function getSelectedProductFromWeb(forceRefresh) {
+      return this.getProducts(forceRefresh).then((productsList) => {
+        const name = $stateParams.productId ? $stateParams.productId : '';
+        const type = $rootScope.currentSectionInformation
+          ? $rootScope.currentSectionInformation.toUpperCase()
+          : null;
 
-      let p = null;
+        let p = null;
 
-      _.forEach(productsList, (product) => {
-        if (product.name === name && product.type === type) {
-          p = product;
-        }
-        if (product.hasSubComponent === true && !p) {
-          p = _.find(product.subProducts, { name, type });
-        }
+        _.forEach(productsList, (product) => {
+          if (product.name === name && product.type === type) {
+            p = product;
+          }
+          if (product.hasSubComponent === true && !p) {
+            p = _.find(product.subProducts, { name, type });
+          }
+        });
+
+        return p;
       });
+    };
 
-      return p;
-    });
-  };
-
-  /**
-     * Get working-status for the specified product
-     */
-  this.getWorks = function (category, affiliated, active) {
-    return $http
-      .get(`${constants.aapiRootPath}working-status/${category}`, {
-        params: {
-          affiliated,
-          active,
-        },
-      })
-      .then(resp => resp.data);
-  };
-
-  this.getProducts(true);
-});
+    this.getProducts(true);
+  });
