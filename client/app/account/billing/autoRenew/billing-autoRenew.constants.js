f2883acd43dc630b4097b34c5e18bb66c14c6c1e
Marie JONES <marie.jones@corp.ovh.com>
2018-11-14 13:09:38 +0100

    feat(billing.autorenew): disable renew action for in debt services
    MANAGER-1802
    MANAGER-1806

--- client/app/account/billing/autoRenew/billing-autoRenew.constants.js
+++ client/app/account/billing/autoRenew/billing-autoRenew.constants.js
@@ -1,3 +1,4 @@
 angular
   .module('Billing.constants')
-  .constant('SUBSIDIARIES_WITH_RECENT_AUTORENEW', ['FR', 'CA', 'QC', 'WE', 'WS', 'ASIA', 'SG', 'AU']);
+  .constant('SUBSIDIARIES_WITH_RECENT_AUTORENEW', ['FR', 'CA', 'QC', 'WE', 'WS', 'ASIA', 'SG', 'AU'])
+  .constant('DEBT_STATUS', ['PENDING_DEBT', 'UN_PAID']);
