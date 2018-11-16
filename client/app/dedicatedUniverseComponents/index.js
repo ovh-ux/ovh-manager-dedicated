444dd76390caf1144d8170f31998e4c3487ca606
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-11-16 17:43:29 +0100

    refactor(components.bandwidth): move to dedicatedUniverseComponent

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
+import ducBandwidth from './bandwidth';
+
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducBandwidth,
   ]);
 
 export default moduleName;
