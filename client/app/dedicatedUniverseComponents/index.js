214e13e181d4e081cac57e2cd38999198c00367a
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-11-19 10:24:02 +0100

    refactor(components.bytes): move to dedicatedUniverseComponent

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
+import ducBytes from './bytes';
+
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducBytes,
   ]);
 
 export default moduleName;
