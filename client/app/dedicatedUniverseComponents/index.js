4644242326f925a26a30ba785effc0b93ba78938
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-11-19 14:22:38 +0100

    refactor(components.translate): move to dedicatedUniverseComponent

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
+import ducTranslate from './translate';
+
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducTranslate,
   ]);
 
 export default moduleName;
