92c4264cee48f9eb8159e9e9798efc78539c5282
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2018-11-26 08:17:25 +0100

    refactor(components.bandwidth): move to dedicatedUniverseComponent
    ref: OM-190

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 
+import ducBandwidth from './bandwidth';
 import ducBytes from './bytes';
 
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducBandwidth,
     ducBytes,
   ]);
 
