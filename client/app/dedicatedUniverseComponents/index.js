60c8cef515fe74e4de7674adef7349e1602c99dc
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2018-11-27 08:13:20 +0100

    refactor(components.contract): move to dedicatedUniverseComponent
    ref: OM-192

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 import ducBandwidth from './bandwidth';
 import ducBytes from './bytes';
+import ducContract from './contract';
 
 const moduleName = 'dedicatedUniverseComponents';
 
@@ -9,6 +10,7 @@ angular
   .module(moduleName, [
     ducBandwidth,
     ducBytes,
+    ducContract,
   ]);
 
 export default moduleName;
