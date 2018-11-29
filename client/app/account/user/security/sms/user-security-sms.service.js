0cd84a6cb42ffb655bc9ef61aa00f1302ed99d72
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-11-29 16:02:20 +0100

    fix(account.user.security.sms): re-enable sms

--- client/app/account/user/security/sms/user-security-sms.service.js
+++ client/app/account/user/security/sms/user-security-sms.service.js
@@ -1,131 +1,153 @@
-angular.module('UserAccount').service('UserAccount.services.doubleAuth.sms', (
-  $q,
-  OvhHttp,
-  constants,
-) => {
-  /**
-     *  Get SMS accounts ids.
-     * @return {Promise}
-     */
-  this.query = () => OvhHttp.get('/me/accessRestriction/sms', {
-    rootPath: 'apiv6',
-  });
+angular.module('UserAccount').service('UserAccount.services.doubleAuth.sms', class UserAccountServicesDoubleAuthSms {
+  constructor($q, OvhHttp, constants) {
+    this.$q = $q;
+    this.OvhHttp = OvhHttp;
+    this.constants = constants;
+  }
 
   /**
-     *  Get SMS account.
-     * @param  {Integer} id
-     * @return {Promise}
-     */
-  this.get = id => OvhHttp.get('/me/accessRestriction/sms/{id}', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-  });
+   *  Get SMS accounts ids.
+   * @return {Promise}
+   */
+  query() {
+    return this.OvhHttp.get('/me/accessRestriction/sms', {
+      rootPath: 'apiv6',
+    });
+  }
 
   /**
-     * Add new SMS account.
-     * @param  {String} phone international phone number.
-     * @return {Promise}
-     */
-  this.post = phone => OvhHttp.post('/me/accessRestriction/sms', {
-    rootPath: 'apiv6',
-    data: {
-      phone,
-    },
-  });
+   *  Get SMS account.
+   * @param  {Integer} id
+   * @return {Promise}
+   */
+  get(id) {
+    return this.OvhHttp.get('/me/accessRestriction/sms/{id}', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+    });
+  }
 
   /**
-     * Edit description from a given SMS account.
-     * @param  {Integer} id
-     * @param  {String} description
-     * @return {Promise}
-     */
-  this.edit = (id, description) => OvhHttp.put('/me/accessRestriction/sms/{id}', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-    data: {
-      description,
-    },
-  });
+   * Add new SMS account.
+   * @param  {String} phone international phone number.
+   * @return {Promise}
+   */
+  post(phone) {
+    return this.OvhHttp.post('/me/accessRestriction/sms', {
+      rootPath: 'apiv6',
+      data: {
+        phone,
+      },
+    });
+  }
 
   /**
-     * Delete a given SMS account.
-     * @param  {Integer} id
-     * @return {Promise}
-     */
-  this.delete = id => OvhHttp.delete('/me/accessRestriction/sms/{id}', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-  });
+   * Edit description from a given SMS account.
+   * @param  {Integer} id
+   * @param  {String} description
+   * @return {Promise}
+   */
+  edit(id, description) {
+    return this.OvhHttp.put('/me/accessRestriction/sms/{id}', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+      data: {
+        description,
+      },
+    });
+  }
 
   /**
-     * Disable a given SMS account.
-     * @param  {Integer} id
-     * @param  {String} code
-     * @return {Promise}
-     */
-  this.disable = (id, code) => OvhHttp.post('/me/accessRestriction/sms/{id}/disable', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-    data: {
-      code,
-    },
-  });
+   * Delete a given SMS account.
+   * @param  {Integer} id
+   * @return {Promise}
+   */
+  delete(id) {
+    return this.OvhHttp.delete('/me/accessRestriction/sms/{id}', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+    });
+  }
 
   /**
-     * Enable a given SMS account.
-     * @param  {Integer} id
-     * @param  {String} code
-     * @return {Promise}
-     */
-  this.enable = (id, code) => OvhHttp.post('/me/accessRestriction/sms/{id}/enable', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-    data: {
-      code,
-    },
-  });
+   * Disable a given SMS account.
+   * @param  {Integer} id
+   * @param  {String} code
+   * @return {Promise}
+   */
+  disable(id, code) {
+    return this.OvhHttp.post('/me/accessRestriction/sms/{id}/disable', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+      data: {
+        code,
+      },
+    });
+  }
 
   /**
-     * Send code to a given SMS account.
-     * @param  {Integer} id
-     * @return {Promise}
-     */
-  this.sendCode = id => OvhHttp.post('/me/accessRestriction/sms/{id}/sendCode', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-  });
+   * Enable a given SMS account.
+   * @param  {Integer} id
+   * @param  {String} code
+   * @return {Promise}
+   */
+  enable(id, code) {
+    return this.OvhHttp.post('/me/accessRestriction/sms/{id}/enable', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+      data: {
+        code,
+      },
+    });
+  }
 
   /**
-     * Validate a given SMS account.
-     * @param  {Integer} id
-     * @param  {String} code
-     * @return {Promise}
-     */
-  this.validate = (id, code) => OvhHttp.post('/me/accessRestriction/sms/{id}/validate', {
-    rootPath: 'apiv6',
-    urlParams: {
-      id,
-    },
-    data: {
-      code,
-    },
-  });
+   * Send code to a given SMS account.
+   * @param  {Integer} id
+   * @return {Promise}
+   */
+  sendCode(id) {
+    return this.OvhHttp.post('/me/accessRestriction/sms/{id}/sendCode', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+    });
+  }
 
   /**
-     * Does double auth SMS is supported.
-     * @return {Boolean}
-     */
-  this.isSupported = (constants.target === 'EU');
+   * Validate a given SMS account.
+   * @param  {Integer} id
+   * @param  {String} code
+   * @return {Promise}
+   */
+  validate(id, code) {
+    return this.OvhHttp.post('/me/accessRestriction/sms/{id}/validate', {
+      rootPath: 'apiv6',
+      urlParams: {
+        id,
+      },
+      data: {
+        code,
+      },
+    });
+  }
+
+  /**
+   * Does double auth SMS is supported.
+   * @return {Boolean}
+   */
+  isSupported() {
+    return this.constants.target === 'EU';
+  }
 });
