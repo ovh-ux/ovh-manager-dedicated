7cb9725880f2d2149c03ebb88a6170dfedd86ad8
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-12-12 17:21:42 +0100

    feat(): add payment method component

--- client/app/account/billing/payment/method/add/billing-payment-method-add.controller.js
+++ client/app/account/billing/payment/method/add/billing-payment-method-add.controller.js
@@ -1,8 +1,26 @@
 export default class BillingPaymentMethodAddCtrl {
   /* @ngInject */
 
-  constructor($state) {
+  constructor($state, ovhPaymentMethod) {
     // dependencies injections
     this.$state = $state;
+    this.ovhPaymentMethod = ovhPaymentMethod;
+
+    // other attributes used in view
+    this.loading = {
+      init: false,
+    };
   }
+
+  /* =====================================
+  =            INITIALIZATION            =
+  ====================================== */
+
+  $onInit() {
+    this.loading.init = true;
+
+    return this.ovhPaymentMethod.getAvailablePaymentTypes();
+  }
+
+  /* -----  End of INITIALIZATION  ------ */
 }
