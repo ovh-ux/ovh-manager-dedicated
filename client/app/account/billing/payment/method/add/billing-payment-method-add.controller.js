f00554730321b1a0d55c565ce4383dc6b1af6221
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2018-12-13 17:29:55 +0100

    feat(billing payment): wip add payment methods

--- client/app/account/billing/payment/method/add/billing-payment-method-add.controller.js
+++ client/app/account/billing/payment/method/add/billing-payment-method-add.controller.js
@@ -1,16 +1,38 @@
 export default class BillingPaymentMethodAddCtrl {
   /* @ngInject */
 
-  constructor($state, ovhPaymentMethod) {
+  constructor($q, $state, ovhPaymentMethod, paymentMethodListResolve) {
     // dependencies injections
+    this.$q = $q;
     this.$state = $state;
     this.ovhPaymentMethod = ovhPaymentMethod;
+    this.paymentMethodListResolve = paymentMethodListResolve;
 
     // other attributes used in view
     this.loading = {
       init: false,
+      add: false,
     };
+
+    this.model = {
+      selectedPaymentMethodType: null,
+    };
+
+    this.paymentMethodTypes = null;
   }
+
+  /* ==============================
+  =            Events            =
+  ============================== */
+
+  onPayentMethodAddBtnClick() {
+    console.log('onPayentMethodAddBtnClick');
+    this.loading.add = true;
+
+    // return this.ovhPaymentMethod.add
+  }
+
+  /* =====  End of Events  ====== */
 
   /* =====================================
   =            INITIALIZATION            =
@@ -19,7 +41,16 @@ export default class BillingPaymentMethodAddCtrl {
   $onInit() {
     this.loading.init = true;
 
-    return this.ovhPaymentMethod.getAvailablePaymentTypes();
+    return this.$q.all({
+      paymentMethods: this.paymentMethodListResolve.promise,
+      paymentMethodTypes: this.ovhPaymentMethod.getAvailablePaymentMethodTypes(),
+    }).then(({ paymentMethods, paymentMethodTypes }) => {
+      this.hasPaymentMethod = paymentMethods.length > 0;
+      this.paymentMethodTypes = paymentMethodTypes;
+    })
+      .finally(() => {
+        this.loading.init = false;
+      });
   }
 
   /* -----  End of INITIALIZATION  ------ */
