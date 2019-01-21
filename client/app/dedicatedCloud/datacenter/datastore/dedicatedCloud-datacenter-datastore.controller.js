82daeebafb64a901c746fe4f5f883abcef6c4291
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-21 16:20:59 +0100

    refactor(dedicated-cloud-datacenter-datastore): refactor controller

--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
@@ -1,47 +1,99 @@
-angular.module('App').controller('DedicatedCloudSubDatacentersDatastoreCtrl', function ($q, $scope, $state, $stateParams, constants, DedicatedCloud) {
-  $scope.constants = constants;
-  const noConsumptionResponse = new RegExp('no consumption', 'i');
+angular
+  .module('App')
+  .controller(
+    'DedicatedCloudSubDatacentersDatastoreCtrl',
+    class {
+      /* @ngInject */
+      constructor(
+        $q,
+        $scope,
+        $state,
+        $stateParams,
 
-  this.loadDatastores = ({ offset, pageSize }) => DedicatedCloud.getDatastores(
-    $stateParams.productId,
-    $stateParams.datacenterId,
-    pageSize,
-    offset - 1,
-  ).then((result) => {
-    const datastores = _.get(result, 'list.results');
-    return $q.all(datastores.map((dc) => {
-      if (dc.billing === 'HOURLY') {
-        return DedicatedCloud.getDatastoreHourlyConsumption(
-          $stateParams.productId,
-          $stateParams.datacenterId,
-          dc.id,
-        ).then((response) => {
-          _.set(dc, 'consumption', _.get(response, 'consumption.value'));
-          _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
-          return dc;
-        }).catch((err) => {
-          if (noConsumptionResponse.test(err.message)) {
-            _.set(dc, 'consumption', 0);
-          } else {
-            _.set(dc, 'consumption', null);
-          }
-          return dc;
-        });
+        constants,
+        DedicatedCloud,
+      ) {
+        this.$q = $q;
+        this.$scope = $scope;
+        this.$state = $state;
+        this.$stateParams = $stateParams;
+
+        this.constants = constants;
+        this.DedicatedCloud = DedicatedCloud;
       }
-      return $q.when(dc);
-    })).then(data => ({
-      data,
-      meta: {
-        totalCount: result.count,
-      },
-    }));
-  });
 
-  this.orderDatastore = (datacenter) => {
-    if (constants.target === 'US') {
-      $state.go('app.dedicatedClouds.datacenter.datastores.orderUS');
-    } else {
-      $scope.setAction('datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order', datacenter.model, true);
-    }
-  };
-});
+      $onInit() {
+        this.$scope.constants = this.constants;
+      }
+
+      loadDatastores({
+        offset,
+        pageSize,
+      }) {
+        const noConsumptionResponse = new RegExp(
+          'no consumption',
+          'i',
+        );
+
+        return this.DedicatedCloud
+          .getDatastores(
+            this.$stateParams.productId,
+            this.$stateParams.datacenterId,
+            pageSize,
+            offset - 1,
+          )
+          .then((result) => {
+            const datastores = _.get(
+              result,
+              'list.results',
+            );
+
+            return this.$q
+              .all(datastores.map((dc) => {
+                if (dc.billing === 'HOURLY') {
+                  return this.DedicatedCloud
+                    .getDatastoreHourlyConsumption(
+                      this.$stateParams.productId,
+                      this.$stateParams.datacenterId,
+                      dc.id,
+                    )
+                    .then((response) => {
+                      _.set(dc, 'consumption', _.get(response, 'consumption.value'));
+                      _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
+
+                      return dc;
+                    })
+                    .catch((err) => {
+                      if (noConsumptionResponse.test(err.message)) {
+                        _.set(dc, 'consumption', 0);
+                      } else {
+                        _.set(dc, 'consumption', null);
+                      }
+
+                      return dc;
+                    });
+                }
+                return this.$q.when(dc);
+              }))
+              .then(data => ({
+                data,
+                meta: {
+                  totalCount: result.count,
+                },
+              }));
+          });
+      }
+
+      orderDatastore(datacenter) {
+        if (this.constants.target === 'US') {
+          this.$state.go('app.dedicatedClouds.datacenter.datastores.orderUS');
+        } else {
+          this.$scope.setAction(
+            'datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order',
+            datacenter.model,
+            true,
+          );
+        }
+      }
+    },
+  );
