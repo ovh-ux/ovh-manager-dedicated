773982843b336e7260b07da705f84070771b5028
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-22 15:29:20 +0100

    refactor(dedicated-cloud): move constants to own file
    * reindent the constant file

--- client/app/dedicatedCloud/dedicatedCloud.constant.js
+++ client/app/dedicatedCloud/dedicatedCloud.constant.js
@@ -1,14 +1,40 @@
-angular.module('App')
-  .constant('DEDICATED_CLOUD_CONSTANTS', {
-    securityOptions: ['pcidss', 'hds', 'hipaa'],
-  })
-  .constant('VM_ENCRYPTION_KMS', {
-    regex: {
-      ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
-      sslThumbprint: /^(((([0-9]|[a-fA-F]){2}:){19})|((([0-9]|[a-fA-F]){2}:){31}))([0-9]|[a-fA-F]){2}$/,
+angular
+  .module('App')
+  .constant(
+    'DEDICATED_CLOUD_CONSTANTS',
+    {
+      securityOptions: ['pcidss', 'hds', 'hipaa'],
     },
-    creationTaskWaitingConfiguration: 'Waiting for customer to configure the KMS Server...',
-    waitingStatus: ['doing', 'todo'],
-    endStatus: ['canceled', 'done'],
-    pollingDelay: 2000,
-  });
+  )
+  .constant(
+    'VM_ENCRYPTION_KMS',
+    {
+      regex: {
+        ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+        sslThumbprint: /^(((([0-9]|[a-fA-F]){2}:){19})|((([0-9]|[a-fA-F]){2}:){31}))([0-9]|[a-fA-F]){2}$/,
+      },
+      creationTaskWaitingConfiguration: 'Waiting for customer to configure the KMS Server...',
+      waitingStatus: ['doing', 'todo'],
+      endStatus: ['canceled', 'done'],
+      pollingDelay: 2000,
+    },
+  )
+  .constant(
+    'VEEAM_STATE_ENUM',
+    {
+      ENABLED: 'enabled',
+      DISABLED: 'disabled',
+      DISABLING: 'DISABLING',
+      ENABLING: 'ENABLING',
+      ERROR: 'ERROR',
+      REMOVING: 'REMOVING',
+    },
+  )
+  .constant(
+    'COMMERCIAL_RANGE_ENUM',
+    {
+      '2014v1Infrastructure': '2014 Infrastructure',
+      '2014v1Enterprise': '2014 Enterprise',
+      '2013v1': '2013',
+    },
+  );
