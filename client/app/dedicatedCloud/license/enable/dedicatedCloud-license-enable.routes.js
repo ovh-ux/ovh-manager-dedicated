f1cf9e4182829a9473d0a10d5f9460e7372ba7ad
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-23 09:32:24 +0100

    refactor(dedicated-cloud-licence-enable): rewrite `Config`

--- client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
+++ client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
@@ -1,11 +1,19 @@
-angular.module('App').config(($stateProvider, constants) => {
-  if (constants.target === 'US') {
-    $stateProvider.state('app.dedicatedClouds.license.enable', {
-      url: '/enable',
-      templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
-      controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
-      controllerAs: '$ctrl',
-      layout: 'modal',
-    });
-  }
-});
+angular
+  .module('App')
+  .config((
+    $stateProvider,
+    constants,
+  ) => {
+    if (constants.target === 'US') {
+      $stateProvider.state(
+        'app.dedicatedClouds.license.enable',
+        {
+          url: '/enable',
+          templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
+          controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
+          controllerAs: '$ctrl',
+          layout: 'modal',
+        },
+      );
+    }
+  });
