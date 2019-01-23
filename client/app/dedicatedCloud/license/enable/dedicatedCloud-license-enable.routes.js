57f907c955db061d9b49f4cf9e63e185fde7c6a8
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-23 09:35:44 +0100

    feat(dedicated-cloud-license-enable): allow access to route outside US

--- client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
+++ client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
@@ -2,18 +2,15 @@ angular
   .module('App')
   .config((
     $stateProvider,
-    constants,
   ) => {
-    if (constants.target === 'US') {
-      $stateProvider.state(
-        'app.dedicatedClouds.license.enable',
-        {
-          url: '/enable',
-          templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
-          controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
-          controllerAs: '$ctrl',
-          layout: 'modal',
-        },
-      );
-    }
+    $stateProvider.state(
+      'app.dedicatedClouds.license.enable',
+      {
+        url: '/enable',
+        templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
+        controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
+        controllerAs: '$ctrl',
+        layout: 'modal',
+      },
+    );
   });
