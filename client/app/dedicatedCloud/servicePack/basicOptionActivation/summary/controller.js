421713d0be045b60c9e9bd8930bb1bcb9cda3a33
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-02-13 10:42:06 +0100

    feat(dedicated-cloud-service-pack): enhance accessibility

--- client/app/dedicatedCloud/servicePack/basicOptionActivation/summary/controller.js
+++ client/app/dedicatedCloud/servicePack/basicOptionActivation/summary/controller.js
@@ -2,12 +2,18 @@
 export default class DedicatedCloudServicePackBasicOptionActivationSummary {
   constructor(
     $state,
+    ovhUserPref,
   ) {
     this.$state = $state;
+    this.ovhUserPref = ovhUserPref;
   }
 
   $onInit() {
     if (this.orderedServicePack == null) {
+      return this.$state.go('app.dedicatedClouds.servicePackBasicOptionActivation.confirmation');
+    }
+
+    if (this.orderURL == null) {
       return this.$state.go('app.dedicatedClouds.servicePackBasicOptionActivation.confirmation');
     }
 
