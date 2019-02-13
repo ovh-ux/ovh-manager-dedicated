ff2af3682a82981e1094ed9c052f929348cf101a
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-02-13 17:43:19 +0100

    fix(dedicated.server): add alert on task tab if service is expired

--- client/app/dedicated/server/task/dedicated-server-task.controller.js
+++ client/app/dedicated/server/task/dedicated-server-task.controller.js
@@ -1,14 +1,19 @@
 angular.module('App')
   .controller('TaskCtrl', class DedicatedServerTaskController {
-    constructor($scope, $stateParams, Alerter, Server, TASK_STATUS) {
+    constructor($scope, $stateParams, $translate, Alerter, Server) {
       this.$scope = $scope;
       this.$stateParams = $stateParams;
+      this.$translate = $translate;
       this.Alerter = Alerter;
       this.Server = Server;
-      this.TASK_STATUS = TASK_STATUS;
+    }
+
+    $onInit() {
+      this.isServiceExpired = false;
     }
 
     loadDatagridTasks({ offset, pageSize }) {
+      const serviceExpiredErrorCode = 460;
       return this.Server.getTasks(this.$stateParams.productId, pageSize, offset - 1)
         .then(result => ({
           data: _.get(result, 'list.results'),
@@ -16,8 +21,13 @@ angular.module('App')
             totalCount: result.count,
           },
         }))
-        .catch((err) => {
-          this.Alerter.alertFromSWS(this.$translate.instant('server_configuration_task_loading_error'), err, 'taskAlert');
+        .catch((error) => {
+          if (error.code === serviceExpiredErrorCode) {
+            this.isServiceExpired = true;
+            this.Alerter.error(this.$translate.instant('server_configuration_service_expired'), 'taskAlert');
+          } else {
+            this.Alerter.alertFromSWS(this.$translate.instant('server_configuration_task_loading_error'), error, 'dedicatedServerTaskAlert');
+          }
         });
     }
   });
