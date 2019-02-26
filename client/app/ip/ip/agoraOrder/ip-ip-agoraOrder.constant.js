44edb0130f94cc9e99ffd89dcefda88f2e059512
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-02-26 17:27:12 +0100

    refactor(ip): turn Controller into a Class

---
+++ client/app/ip/ip/agoraOrder/ip-ip-agoraOrder.constant.js
@@ -0,0 +1,14 @@
+export const PRODUCT_TYPES = {
+  dedicatedServer: {
+    apiTypeName: 'SERVER',
+    typeName: 'DEDICATED',
+  },
+  privateCloud: {
+    apiTypeName: 'PRIVATE_CLOUD',
+    typeName: 'PRIVATE_CLOUD',
+  },
+};
+
+export default {
+  PRODUCT_TYPES,
+};
--- client/app/ip/ip/agoraOrder/ip-ip-agoraOrder.service.js
+++ client/app/ip/ip/agoraOrder/ip-ip-agoraOrder.service.js
@@ -1,54 +1,62 @@
-angular.module('Module.ip.services').service('IpAgoraOrder', function ($q, OvhHttp) {
-  this.getServices = function () {
-    // dedicated servers
-    const servers = OvhHttp.get('/products', {
-      rootPath: '2api',
-      params: {
-        product: 'SERVER',
-      },
-    }).then((data) => {
-      if (data.errors.length > 0) {
-        return $q.reject(data.errors);
+import { PRODUCT_TYPES } from './ip-ip-agoraOrder.constant';
+
+angular
+  .module('Module.ip.services')
+  .service('IpAgoraOrder', class {
+    constructor(
+      $q,
+      OvhHttp,
+    ) {
+      this.$q = $q;
+      this.OvhHttp = OvhHttp;
+    }
+
+    handleErrorOrServices({ errors, results }) {
+      if (_.isArray(errors) && !_.isEmpty(errors)) {
+        return this.$q.reject(errors);
       }
-      return data.results.length ? data.results[0].services : [];
-    });
 
-    // private cloud
-    const pcc = OvhHttp.get('/products', {
-      rootPath: '2api',
-      params: {
-        product: 'DEDICATED_CLOUD',
-      },
-    }).then((data) => {
-      if (data.errors.length > 0) {
-        return $q.reject(data.errors);
-      }
-      return data.results.length ? data.results[0].services : [];
-    });
+      return _.get(results, '[0].services', []);
+    }
 
-    // merge results & set type
-    return $q.all({ servers, pcc }).then((result) => {
-      _.set(result, 'servers', _.map(result.servers, (item) => {
-        _.set(item, 'type', 'DEDICATED');
-        return item;
-      }));
-      _.set(result, 'pcc', _.map(result.pcc, (item) => {
-        _.set(item, 'type', 'PRIVATE_CLOUD');
-        return item;
-      }));
-      return _.merge(result.servers, result.pcc);
-    });
-  };
+    fetchProducts(product) {
+      return this.OvhHttp
+        .get('/products', {
+          rootPath: '2api',
+          params: {
+            product,
+          },
+        });
+    }
 
-  this.getIpOffers = function () {
-    return OvhHttp.get('/order/catalog/formatted/{catalogName}', {
-      rootPath: 'apiv6',
-      urlParams: {
-        catalogName: 'ip',
-      },
-      params: {
-        ovhSubsidiary: 'US',
-      },
-    }).then(data => data.plans);
-  };
-});
+    getServices() {
+      return this.$q
+        .all([
+          this.fetchProducts(PRODUCT_TYPES.privateCloud.apiTypeName)
+            .then(this.handleErrorOrServices),
+          this.fetchProducts(PRODUCT_TYPES.dedicatedServer.apiTypeName)
+            .then(this.handleErrorOrServices),
+        ])
+        .then(([pccProducts, serverProducts]) => [
+          ...pccProducts.map(product => ({
+            ...product,
+            type: PRODUCT_TYPES.privateCloud.typeName,
+          })),
+          ...serverProducts.map(product => ({
+            ...product,
+            type: PRODUCT_TYPES.dedicatedServer.typeName,
+          })),
+        ]);
+    }
+
+    getIpOffers() {
+      return this.OvhHttp
+        .get('/order/catalog/formatted/ip', {
+          rootPath: 'apiv6',
+          params: {
+            ovhSubsidiary: 'US',
+          },
+        })
+        .then(({ plans }) => plans);
+    }
+  });
