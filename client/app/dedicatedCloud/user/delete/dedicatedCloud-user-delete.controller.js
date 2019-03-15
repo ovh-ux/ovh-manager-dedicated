5ef15ebc02d93ec1621fad210ce835ca0b521144
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 14:22:29 +0100

    refactor(dedicated-cloud-user-delete): turn into class

--- client/app/dedicatedCloud/user/delete/dedicatedCloud-user-delete.controller.js
+++ client/app/dedicatedCloud/user/delete/dedicatedCloud-user-delete.controller.js
@@ -1,19 +1,37 @@
 angular
   .module('App')
-  .controller('DedicatedCloudUserDeleteCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
-    $scope.user = $scope.currentActionData;
-    $scope.deleteUser = function () {
-      $scope.resetAction();
-      DedicatedCloud.deleteUser($stateParams.productId, $scope.user.userId).then(
+  .controller('DedicatedCloudUserDeleteCtrl', class {
+    constructor(
+      $scope,
+      $stateParams,
+      $translate,
+      DedicatedCloud,
+    ) {
+      this.$scope = $scope;
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.DedicatedCloud = DedicatedCloud;
+    }
+
+    $onInit() {
+      this.$scope.user = this.$scope.currentActionData;
+
+      this.$scope.deleteUser = () => this.deleteUser();
+    }
+
+    deleteUser() {
+      this.$scope.resetAction();
+
+      this.DedicatedCloud.deleteUser(this.$stateParams.productId, this.$scope.user.userId).then(
         () => {
-          $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_success', { t0: $scope.user.name }));
+          this.$scope.setMessage(this.$translate.instant('dedicatedCloud_USER_delete_success', { t0: this.$scope.user.name }));
         },
         (data) => {
-          $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_fail', { t0: $scope.user.name }), {
+          this.$scope.setMessage(this.$translate.instant('dedicatedCloud_USER_delete_fail', { t0: this.$scope.user.name }), {
             type: 'ERROR',
             message: data.message,
           });
         },
       );
-    };
+    }
   });
