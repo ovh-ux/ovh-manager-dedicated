d6a7bb23dbd6a12bfb6536c4fdaab72a51fd2377
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 13:59:52 +0100

    style(dedicated-cloud-user-disable): change indentation

--- client/app/dedicatedCloud/user/disable/dedicatedCloud-user-disable.controller.js
+++ client/app/dedicatedCloud/user/disable/dedicatedCloud-user-disable.controller.js
@@ -1,14 +1,16 @@
-angular.module('App').controller('DedicatedCloudUserDisableCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
-  $scope.user = $scope.currentActionData;
-  $scope.disableUser = function () {
-    $scope.resetAction();
-    DedicatedCloud.disableUser($stateParams.productId, $scope.user.userId).then(
-      () => {
-        $scope.setMessage($translate.instant('dedicatedCloud_USER_disable_success', { t0: $scope.user.name }));
-      },
-      (err) => {
-        $scope.setMessage($translate.instant('dedicatedCloud_USER_disable_fail', { t0: $scope.user.name }), err.data);
-      },
-    );
-  };
-});
+angular
+  .module('App')
+  .controller('DedicatedCloudUserDisableCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
+    $scope.user = $scope.currentActionData;
+    $scope.disableUser = function () {
+      $scope.resetAction();
+      DedicatedCloud.disableUser($stateParams.productId, $scope.user.userId).then(
+        () => {
+          $scope.setMessage($translate.instant('dedicatedCloud_USER_disable_success', { t0: $scope.user.name }));
+        },
+        (err) => {
+          $scope.setMessage($translate.instant('dedicatedCloud_USER_disable_fail', { t0: $scope.user.name }), err.data);
+        },
+      );
+    };
+  });
