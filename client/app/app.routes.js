31588660d4dc5d901f143487a1c944c7cc5ae31b
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 13:47:59 +0100

    style(app): change indentation

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,26 +1,28 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app', {
-    abstract: true,
-    url: '',
-    controller: 'AppCtrl',
-    controllerAs: 'AppCtrl',
-    templateUrl: 'app.html',
-    translations: ['common', 'double-authentication', 'user-contracts'],
-    resolve: {
-      currentUser: User => User.getUser(),
-    },
-  });
+angular
+  .module('App')
+  .config(($stateProvider) => {
+    $stateProvider.state('app', {
+      abstract: true,
+      url: '',
+      controller: 'AppCtrl',
+      controllerAs: 'AppCtrl',
+      templateUrl: 'app.html',
+      translations: ['common', 'double-authentication', 'user-contracts'],
+      resolve: {
+        currentUser: User => User.getUser(),
+      },
+    });
 
-  // CDN & NAS
-  $stateProvider.state('app.networks', {
-    url: '/configuration',
-    template: '<ui-view />',
-    abstract: true,
-  });
+    // CDN & NAS
+    $stateProvider.state('app.networks', {
+      url: '/configuration',
+      template: '<ui-view />',
+      abstract: true,
+    });
 
-  // Microsoft
-  $stateProvider.state('app.microsoft', {
-    abstract: true,
-    template: '<ui-view />',
+    // Microsoft
+    $stateProvider.state('app.microsoft', {
+      abstract: true,
+      template: '<ui-view />',
+    });
   });
-});
