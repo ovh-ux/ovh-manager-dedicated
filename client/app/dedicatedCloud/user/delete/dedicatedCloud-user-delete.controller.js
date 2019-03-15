861f9e9e1323f3e624bf2c02fb9d2ca0dba69463
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 14:20:33 +0100

    style(dedicated-cloud-user-delete): change indentation

--- client/app/dedicatedCloud/user/delete/dedicatedCloud-user-delete.controller.js
+++ client/app/dedicatedCloud/user/delete/dedicatedCloud-user-delete.controller.js
@@ -1,17 +1,19 @@
-angular.module('App').controller('DedicatedCloudUserDeleteCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
-  $scope.user = $scope.currentActionData;
-  $scope.deleteUser = function () {
-    $scope.resetAction();
-    DedicatedCloud.deleteUser($stateParams.productId, $scope.user.userId).then(
-      () => {
-        $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_success', { t0: $scope.user.name }));
-      },
-      (data) => {
-        $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_fail', { t0: $scope.user.name }), {
-          type: 'ERROR',
-          message: data.message,
-        });
-      },
-    );
-  };
-});
+angular
+  .module('App')
+  .controller('DedicatedCloudUserDeleteCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
+    $scope.user = $scope.currentActionData;
+    $scope.deleteUser = function () {
+      $scope.resetAction();
+      DedicatedCloud.deleteUser($stateParams.productId, $scope.user.userId).then(
+        () => {
+          $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_success', { t0: $scope.user.name }));
+        },
+        (data) => {
+          $scope.setMessage($translate.instant('dedicatedCloud_USER_delete_fail', { t0: $scope.user.name }), {
+            type: 'ERROR',
+            message: data.message,
+          });
+        },
+      );
+    };
+  });
