0a64591dde12bb06a871a5868496e61891d64394
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 14:12:32 +0100

    style(dedicated-cloud-user-enable): change indentation

--- client/app/dedicatedCloud/user/enable/dedicatedCloud-user-enable.controller.js
+++ client/app/dedicatedCloud/user/enable/dedicatedCloud-user-enable.controller.js
@@ -1,18 +1,20 @@
-angular.module('App').controller('DedicatedCloudUserEnableCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
-  $scope.user = $scope.currentActionData;
+angular
+  .module('App')
+  .controller('DedicatedCloudUserEnableCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
+    $scope.user = $scope.currentActionData;
 
-  $scope.enableUser = function () {
-    DedicatedCloud.enableUser($stateParams.productId, $scope.user.userId)
-      .then(
-        () => {
+    $scope.enableUser = function () {
+      DedicatedCloud.enableUser($stateParams.productId, $scope.user.userId)
+        .then(
+          () => {
           // Start Polling
-        },
-        (err) => {
-          $scope.setMessage($translate.instant('dedicatedCloud_USER_enable_fail', { t0: $scope.user.name }), { message: err.data, type: 'ERROR' });
-        },
-      )
-      .finally(() => {
-        $scope.resetAction();
-      });
-  };
-});
+          },
+          (err) => {
+            $scope.setMessage($translate.instant('dedicatedCloud_USER_enable_fail', { t0: $scope.user.name }), { message: err.data, type: 'ERROR' });
+          },
+        )
+        .finally(() => {
+          $scope.resetAction();
+        });
+    };
+  });
