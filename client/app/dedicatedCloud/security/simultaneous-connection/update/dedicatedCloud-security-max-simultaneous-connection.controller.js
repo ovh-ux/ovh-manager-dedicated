5349b4b005f24244dbd6be6d0f67b16b8745ce1c
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 15:09:37 +0100

    fix(dedicated-cloud-security-max): remove globally exposed class

--- client/app/dedicatedCloud/security/simultaneous-connection/update/dedicatedCloud-security-max-simultaneous-connection.controller.js
+++ client/app/dedicatedCloud/security/simultaneous-connection/update/dedicatedCloud-security-max-simultaneous-connection.controller.js
@@ -1,48 +1,50 @@
-class DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl {
-  constructor(
-    $scope,
-    $stateParams,
-    $translate,
-    DedicatedCloud,
-  ) {
-    this.$scope = $scope;
-    this.$stateParams = $stateParams;
-    this.$translate = $translate;
-    this.DedicatedCloud = DedicatedCloud;
-  }
+{
+  class DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl {
+    constructor(
+      $scope,
+      $stateParams,
+      $translate,
+      DedicatedCloud,
+    ) {
+      this.$scope = $scope;
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.DedicatedCloud = DedicatedCloud;
+    }
 
-  $onInit() {
-    this.maxSimultaneousConnections = {
-      value: null,
-      current: this.$scope.currentActionData,
-    };
+    $onInit() {
+      this.maxSimultaneousConnections = {
+        value: null,
+        current: this.$scope.currentActionData,
+      };
 
-    this.$scope.update = () => this.update();
-  }
+      this.$scope.update = () => this.update();
+    }
 
-  update() {
-    this.$scope.resetAction();
+    update() {
+      this.$scope.resetAction();
 
-    this.DedicatedCloud
-      .updateMaxConcurrentConnections(
-        this.$stateParams.productId,
-        this.maxSimultaneousConnections.value,
-      )
-      .then((data) => {
-        this.$scope.setMessage(this.$translate.instant('dedicatedCloud_SECURITY_change_nb_simultaneous_connection_success'), {
-          ...data,
-          type: 'success',
+      this.DedicatedCloud
+        .updateMaxConcurrentConnections(
+          this.$stateParams.productId,
+          this.maxSimultaneousConnections.value,
+        )
+        .then((data) => {
+          this.$scope.setMessage(this.$translate.instant('dedicatedCloud_SECURITY_change_nb_simultaneous_connection_success'), {
+            ...data,
+            type: 'success',
+          });
+        })
+        .catch((err) => {
+          this.$scope.setMessage(this.$translate.instant('dedicatedCloud_SECURITY_change_nb_simultaneous_connection_failure'), {
+            ...err,
+            type: 'error',
+          });
         });
-      })
-      .catch((err) => {
-        this.$scope.setMessage(this.$translate.instant('dedicatedCloud_SECURITY_change_nb_simultaneous_connection_failure'), {
-          ...err,
-          type: 'error',
-        });
-      });
+    }
   }
+
+  angular
+    .module('App')
+    .controller('DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl', DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl);
 }
-
-angular
-  .module('App')
-  .controller('DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl', DedicatedCloudSecurityMaxSimultaneousConnectionsCtrl);
