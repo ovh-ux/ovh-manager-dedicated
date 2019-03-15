89fb830df83bf232042a5e8232a7424141efa5a7
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 15:15:02 +0100

    refactor(dedicated-cloud-security-timeout): enhance the codebase

--- client/app/dedicatedCloud/security/session-timeout/update/dedicatedCloud-security-session-timeout-update.controller.js
+++ client/app/dedicatedCloud/security/session-timeout/update/dedicatedCloud-security-session-timeout-update.controller.js
@@ -1,33 +1,37 @@
 {
   class DedicatedCloudSecurityUpdateSessionTimeoutCtrl {
-    constructor($scope, $stateParams, DedicatedCloud, $translate) {
+    constructor(
+      $scope,
+      $stateParams,
+      $translate,
+      DedicatedCloud,
+    ) {
       this.$scope = $scope;
       this.$stateParams = $stateParams;
-      this.DedicatedCloud = DedicatedCloud;
       this.$translate = $translate;
+      this.DedicatedCloud = DedicatedCloud;
+    }
 
+    $onInit() {
       this.sessionTimeout = {
         value: null,
-        current: $scope.currentActionData,
+        current: this.$scope.currentActionData,
         never: false,
       };
 
-      $scope.updateSessionTimeout = this.updateSessionTimeout.bind(this);
+      this.$scope.updateSessionTimeout = () => this.updateSessionTimeout();
     }
 
     updateSessionTimeout() {
       this.$scope.resetAction();
-      this.DedicatedCloud.updateSessionExpiration(this.$stateParams.productId, (this.sessionTimeout.never && '0') || this.sessionTimeout.value)
+
+      this.DedicatedCloud
+        .updateSessionExpiration(this.$stateParams.productId, (this.sessionTimeout.never && '0') || this.sessionTimeout.value)
         .then((data) => {
-          this.$scope.setMessage(
-            this.$translate.instant('dedicatedCloud_configuration_SECURITY_update_session_timeout_success'),
-            _.assign(
-              {
-                type: 'success',
-              },
-              data,
-            ),
-          );
+          this.$scope.setMessage(this.$translate.instant('dedicatedCloud_configuration_SECURITY_update_session_timeout_success'), {
+            ...data,
+            type: 'success',
+          });
         })
         .catch((err) => {
           this.$scope.setMessage(this.$translate.instant('dedicatedCloud_configuration_SECURITY_update_session_timeout_fail'), err.data);
@@ -35,5 +39,7 @@
     }
   }
 
-  angular.module('App').controller('DedicatedCloudSecurityUpdateSessionTimeoutCtrl', DedicatedCloudSecurityUpdateSessionTimeoutCtrl);
+  angular
+    .module('App')
+    .controller('DedicatedCloudSecurityUpdateSessionTimeoutCtrl', DedicatedCloudSecurityUpdateSessionTimeoutCtrl);
 }
