261fe45a48ab06dc4b6067b8c8ccb3a865c21826
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-15 14:14:55 +0100

    refactor(dedicated-cloud-user-enable): turn into class

--- client/app/dedicatedCloud/user/enable/dedicatedCloud-user-enable.controller.js
+++ client/app/dedicatedCloud/user/enable/dedicatedCloud-user-enable.controller.js
@@ -1,20 +1,36 @@
 angular
   .module('App')
-  .controller('DedicatedCloudUserEnableCtrl', ($scope, $stateParams, $translate, DedicatedCloud) => {
-    $scope.user = $scope.currentActionData;
+  .controller('DedicatedCloudUserEnableCtrl', class {
+    constructor(
+      $scope,
+      $stateParams,
+      $translate,
+      DedicatedCloud,
+    ) {
+      this.$scope = $scope;
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.DedicatedCloud = DedicatedCloud;
+    }
 
-    $scope.enableUser = function () {
-      DedicatedCloud.enableUser($stateParams.productId, $scope.user.userId)
+    $onInit() {
+      this.$scope.user = this.$scope.currentActionData;
+
+      this.enableUser = () => this.enableUser();
+    }
+
+    enableUser() {
+      this.DedicatedCloud.enableUser(this.$stateParams.productId, this.$scope.user.userId)
         .then(
           () => {
           // Start Polling
           },
           (err) => {
-            $scope.setMessage($translate.instant('dedicatedCloud_USER_enable_fail', { t0: $scope.user.name }), { message: err.data, type: 'ERROR' });
+            this.$scope.setMessage(this.$translate.instant('dedicatedCloud_USER_enable_fail', { t0: this.$scope.user.name }), { message: err.data, type: 'ERROR' });
           },
         )
         .finally(() => {
-          $scope.resetAction();
+          this.$scope.resetAction();
         });
-    };
+    }
   });
