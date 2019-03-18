95cebee8a004983e2fe74266aaac88086eb62ad3
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-18 16:48:27 +0100

    style(backup.enable): change indentation

--- client/app/dedicatedCloud/datacenter/backup/enable/dedicatedCloud-datacenter-backup-enable.controller.js
+++ client/app/dedicatedCloud/datacenter/backup/enable/dedicatedCloud-datacenter-backup-enable.controller.js
@@ -1,77 +1,86 @@
-angular.module('App').controller('DedicatedCloudSubDatacenterVeeamBackupEnableCtrl', class {
-  constructor($q, $stateParams, $state, $translate, Alerter, DedicatedCloud) {
+angular
+  .module('App')
+  .controller('DedicatedCloudSubDatacenterVeeamBackupEnableCtrl', class {
+    constructor(
+      $q,
+      $stateParams,
+      $state,
+      $translate,
+      Alerter,
+      DedicatedCloud,
+    ) {
     // dependencies injections
-    this.$q = $q;
-    this.$stateParams = $stateParams;
-    this.$state = $state;
-    this.$translate = $translate;
-    this.Alerter = Alerter;
-    this.DedicatedCloud = DedicatedCloud;
+      this.$q = $q;
+      this.$stateParams = $stateParams;
+      this.$state = $state;
+      this.$translate = $translate;
+      this.Alerter = Alerter;
+      this.DedicatedCloud = DedicatedCloud;
 
-    // controller attributes
-    this.loading = {
-      init: false,
-      enable: false,
-    };
+      // controller attributes
+      this.loading = {
+        init: false,
+        enable: false,
+      };
 
-    this.datacenter = null;
-    this.hosts = null;
-  }
+      this.datacenter = null;
+      this.hosts = null;
+    }
 
-  /* =============================
+    /* =============================
     =            EVENTS            =
     ============================== */
 
-  onConfirmBtnClick() {
-    if (!this.hosts.length) {
-      return this.onCancelBtnClick();
+    onConfirmBtnClick() {
+      if (!this.hosts.length) {
+        return this.onCancelBtnClick();
+      }
+
+      this.loading.enable = true;
+
+      return this.DedicatedCloud
+        .enableVeeam(this.$stateParams.productId, this.$stateParams.datacenterId)
+        .then(() => {
+          this.Alerter.success(this.$translate.instant('dedicatedCloud_tab_veeam_enable_success', {
+            t0: this.datacenter.name,
+          }), 'dedicatedCloudDatacenterAlert');
+        })
+        .catch((error) => {
+          this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail', {
+            t0: this.datacenter.name,
+          }), error, 'dedicatedCloudDatacenterAlert');
+        })
+        .finally(() => {
+          this.loading.enable = false;
+          this.onCancelBtnClick();
+        });
     }
 
-    this.loading.enable = true;
+    onCancelBtnClick() {
+      this.$state.go('^');
+    }
 
-    return this.DedicatedCloud
-      .enableVeeam(this.$stateParams.productId, this.$stateParams.datacenterId)
-      .then(() => {
-        this.Alerter.success(this.$translate.instant('dedicatedCloud_tab_veeam_enable_success', {
-          t0: this.datacenter.name,
-        }), 'dedicatedCloudDatacenterAlert');
-      })
-      .catch((error) => {
-        this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail', {
-          t0: this.datacenter.name,
-        }), error, 'dedicatedCloudDatacenterAlert');
-      })
-      .finally(() => {
-        this.loading.enable = false;
-        this.onCancelBtnClick();
-      });
-  }
+    /* -----  End of EVENTS  ------ */
 
-  onCancelBtnClick() {
-    this.$state.go('^');
-  }
-
-  /* -----  End of EVENTS  ------ */
-
-  /* =====================================
+    /* =====================================
     =            INITIALIZATION            =
     ====================================== */
 
-  $onInit() {
-    this.loading.init = true;
+    $onInit() {
+      this.loading.init = true;
 
-    return this.$q.all({
-      datacenter: this.DedicatedCloud
-        .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId),
-      hosts: this.DedicatedCloud
-        .getHostsLexi(this.$stateParams.productId, this.$stateParams.datacenterId),
-    }).then((data) => {
-      this.datacenter = data.datacenter;
-      this.hosts = data.hosts;
-    }).finally(() => {
-      this.loading.init = false;
-    });
-  }
+      return this.$q.all({
+        datacenter: this.DedicatedCloud
+          .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId),
+        hosts: this.DedicatedCloud
+          .getHostsLexi(this.$stateParams.productId, this.$stateParams.datacenterId),
+      }).then((data) => {
+        this.datacenter = data.datacenter;
+        this.hosts = data.hosts;
+      }).finally(() => {
+        this.loading.init = false;
+      });
+    }
 
   /* -----  End of INITIALIZATION  ------ */
-});
+  });
