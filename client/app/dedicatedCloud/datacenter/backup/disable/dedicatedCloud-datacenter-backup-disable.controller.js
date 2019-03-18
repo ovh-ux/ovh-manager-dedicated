f463334c40b2577853b077fa880be593b2cc65d0
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-18 16:30:37 +0100

    style(dedicated-cloud-datacenters-backup-disable): change indent

--- client/app/dedicatedCloud/datacenter/backup/disable/dedicatedCloud-datacenter-backup-disable.controller.js
+++ client/app/dedicatedCloud/datacenter/backup/disable/dedicatedCloud-datacenter-backup-disable.controller.js
@@ -1,68 +1,76 @@
-angular.module('App').controller('DedicatedCloudSubDatacenterVeeamBackupDisableCtrl', class {
-  constructor($stateParams, $state, $translate, Alerter, DedicatedCloud) {
+angular
+  .module('App')
+  .controller('DedicatedCloudSubDatacenterVeeamBackupDisableCtrl', class {
+    constructor(
+      $stateParams,
+      $state,
+      $translate,
+      Alerter,
+      DedicatedCloud,
+    ) {
     // dependencies injections
-    this.$stateParams = $stateParams;
-    this.$state = $state;
-    this.$translate = $translate;
-    this.Alerter = Alerter;
-    this.DedicatedCloud = DedicatedCloud;
+      this.$stateParams = $stateParams;
+      this.$state = $state;
+      this.$translate = $translate;
+      this.Alerter = Alerter;
+      this.DedicatedCloud = DedicatedCloud;
 
-    // controller attributes
-    this.loading = {
-      init: false,
-      disable: false,
-    };
+      // controller attributes
+      this.loading = {
+        init: false,
+        disable: false,
+      };
 
-    this.datacenter = null;
-  }
+      this.datacenter = null;
+    }
 
-  /* =============================
+    /* =============================
     =            EVENTS            =
     ============================== */
 
-  onConfirmBtnClick() {
-    this.loading.disable = true;
+    onConfirmBtnClick() {
+      this.loading.disable = true;
 
-    return this.DedicatedCloud
-      .disableVeeam(this.$stateParams.productId, this.$stateParams.datacenterId)
-      .then(() => {
-        this.Alerter.success(this.$translate.instant('dedicatedCloud_tab_veeam_disable_success', {
-          t0: this.datacenter.name,
-        }), 'dedicatedCloudDatacenterAlert');
-      })
-      .catch((error) => {
-        this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_veeam_disable_fail', {
-          t0: this.datacenter.name,
-        }), error, 'dedicatedCloudDatacenterAlert');
-      })
-      .finally(() => {
-        this.loading.disable = false;
-        this.onCancelBtnClick();
-      });
-  }
+      return this.DedicatedCloud
+        .disableVeeam(this.$stateParams.productId, this.$stateParams.datacenterId)
+        .then(() => {
+          this.Alerter.success(this.$translate.instant('dedicatedCloud_tab_veeam_disable_success', {
+            t0: this.datacenter.name,
+          }), 'dedicatedCloudDatacenterAlert');
+        })
+        .catch((error) => {
+          this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_veeam_disable_fail', {
+            t0: this.datacenter.name,
+          }), error, 'dedicatedCloudDatacenterAlert');
+        })
+        .finally(() => {
+          this.loading.disable = false;
+          this.onCancelBtnClick();
+        });
+    }
 
-  onCancelBtnClick() {
-    this.$state.go('^');
-  }
+    onCancelBtnClick() {
+      this.$state.go('^');
+    }
 
-  /* -----  End of EVENTS  ------ */
+    /* -----  End of EVENTS  ------ */
 
-  /* =====================================
+    /* =====================================
     =            INITIALIZATION            =
     ====================================== */
 
-  $onInit() {
-    this.loading.disable = true;
+    $onInit() {
+      this.loading.disable = true;
 
-    return this.DedicatedCloud
-      .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId)
-      .then((datacenter) => {
-        this.datacenter = datacenter;
-      })
-      .finally(() => {
-        this.loading.disable = false;
-      });
-  }
+      return this.DedicatedCloud
+        .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId)
+        .then((datacenter) => {
+          this.datacenter = datacenter;
+        })
+        .finally(() => {
+          this.loading.disable = false;
+        });
+    }
 
   /* -----  End of INITIALIZATION  ------ */
-});
+  });
