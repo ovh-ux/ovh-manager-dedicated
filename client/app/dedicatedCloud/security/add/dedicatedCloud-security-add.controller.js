7271603b112494e6aa31688c0e99e21219372345
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-18 14:48:04 +0100

    refactor(dedicated-cloud-security-add): turn into class

--- client/app/dedicatedCloud/security/add/dedicatedCloud-security-add.controller.js
+++ client/app/dedicatedCloud/security/add/dedicatedCloud-security-add.controller.js
@@ -1,20 +1,38 @@
 angular
   .module('App')
-  .controller('DedicatedCloudSecurityPolicyAddCtrl', ($scope, $stateParams, DedicatedCloud, $translate, REGEX) => {
-    $scope.regex = REGEX;
-    $scope.newNetwork = {
-      value: null,
-    };
+  .controller('DedicatedCloudSecurityPolicyAddCtrl', class {
+    constructor(
+      $scope,
+      $stateParams,
+      DedicatedCloud,
+      $translate,
+      REGEX,
+    ) {
+      this.$scope = $scope;
+      this.$stateParams = $stateParams;
+      this.DedicatedCloud = DedicatedCloud;
+      this.$translate = $translate;
+      this.REGEX = REGEX;
 
-    $scope.addEntry = function () {
-      $scope.resetAction();
-      DedicatedCloud.addSecurityPolicy($stateParams.productId, $scope.newNetwork).then(
-        (data) => {
-          $scope.setMessage($translate.instant('dedicatedCloud_configuration_SECURITY_policy_add_success'), data);
-        },
-        (data) => {
-          $scope.setMessage($translate.instant('dedicatedCloud_configuration_SECURITY_policy_add_fail', [$scope.newNetwork.value]), data.data);
-        },
-      );
-    };
+
+      this.$scope.regex = this.REGEX;
+      this.$scope.newNetwork = {
+        value: null,
+      };
+      this.$scope.addEntry = () => this.addEntry();
+    }
+
+    addEntry() {
+      this.$scope.resetAction();
+
+      this.DedicatedCloud.addSecurityPolicy(this.$stateParams.productId, this.$scope.newNetwork)
+        .then(
+          (data) => {
+            this.$scope.setMessage(this.$translate.instant('dedicatedCloud_configuration_SECURITY_policy_add_success'), data);
+          },
+          (data) => {
+            this.$scope.setMessage(this.$translate.instant('dedicatedCloud_configuration_SECURITY_policy_add_fail', [this.$scope.newNetwork.value]), data.data);
+          },
+        );
+    }
   });
