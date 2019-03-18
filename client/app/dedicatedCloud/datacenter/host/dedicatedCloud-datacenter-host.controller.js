f907594677a141b41339c5e5c8cfb29bf50aa174
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-18 17:23:37 +0100

    style(host): change indentation

--- client/app/dedicatedCloud/datacenter/host/dedicatedCloud-datacenter-host.controller.js
+++ client/app/dedicatedCloud/datacenter/host/dedicatedCloud-datacenter-host.controller.js
@@ -1,50 +1,59 @@
-angular.module('App').controller('DedicatedCloudSubDatacentersHostCtrl', function ($q, $scope, $state, $stateParams, $translate, constants, DedicatedCloud) {
-  this.loadHosts = ({ offset, pageSize }) => DedicatedCloud.getHosts(
-    $stateParams.productId,
-    $stateParams.datacenterId,
-    pageSize,
-    offset - 1,
-  ).then((result) => {
-    const hosts = _.get(result, 'list.results');
-    return $q.all(hosts.map((host) => {
-      if (host.billingType === 'HOURLY') {
-        return DedicatedCloud
-          .getHostHourlyConsumption(
-            $stateParams.productId,
-            $stateParams.datacenterId,
-            host.hostId,
-          )
-          .then(consumption => _.merge({}, host, consumption))
-          .catch(() => host);
+angular
+  .module('App')
+  .controller('DedicatedCloudSubDatacentersHostCtrl', function ($q, $scope, $state, $stateParams, $translate, constants, DedicatedCloud) {
+    this.loadHosts = ({ offset, pageSize }) => DedicatedCloud
+      .getHosts($stateParams.productId, $stateParams.datacenterId, pageSize, offset - 1)
+      .then((result) => {
+        const hosts = _.get(result, 'list.results');
+
+        return $q
+          .all(hosts
+            .map((host) => {
+              if (host.billingType === 'HOURLY') {
+                return DedicatedCloud
+                  .getHostHourlyConsumption(
+                    $stateParams.productId,
+                    $stateParams.datacenterId,
+                    host.hostId,
+                  )
+                  .then(consumption => _.merge({}, host, consumption))
+                  .catch(() => host);
+              }
+
+              return $q.when(host);
+            }))
+          .then(hostsWithConsumption => ({
+            data: hostsWithConsumption,
+            meta: {
+              totalCount: result.count,
+            },
+          }));
+      });
+
+    this.$onInit = () => {
+      this.loading = true;
+      this.constants = constants;
+
+      return DedicatedCloud
+        .getDatacenterInfoProxy(
+          $stateParams.productId,
+          $stateParams.datacenterId,
+        )
+        .then((datacenter) => {
+          $scope.datacenter.model.commercialRangeName = datacenter.commercialRangeName;
+          $scope.datacenter.model.hasDiscountAMD = DedicatedCloud
+            .hasDiscount($scope.datacenter.model);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    };
+
+    this.orderHost = (datacenter) => {
+      if (constants.target === 'US') {
+        $state.go('app.dedicatedClouds.datacenter.hosts.orderUS');
+      } else {
+        $scope.setAction('datacenter/host/order/dedicatedCloud-datacenter-host-order', datacenter.model, true);
       }
-      return $q.when(host);
-    })).then(hostsWithConsumption => ({
-      data: hostsWithConsumption,
-      meta: {
-        totalCount: result.count,
-      },
-    }));
+    };
   });
-
-  this.$onInit = () => {
-    this.loading = true;
-    this.constants = constants;
-    return DedicatedCloud.getDatacenterInfoProxy(
-      $stateParams.productId,
-      $stateParams.datacenterId,
-    ).then((datacenter) => {
-      $scope.datacenter.model.commercialRangeName = datacenter.commercialRangeName;
-      $scope.datacenter.model.hasDiscountAMD = DedicatedCloud.hasDiscount($scope.datacenter.model);
-    }).finally(() => {
-      this.loading = false;
-    });
-  };
-
-  this.orderHost = (datacenter) => {
-    if (constants.target === 'US') {
-      $state.go('app.dedicatedClouds.datacenter.hosts.orderUS');
-    } else {
-      $scope.setAction('datacenter/host/order/dedicatedCloud-datacenter-host-order', datacenter.model, true);
-    }
-  };
-});
