c9727e98d6a6c9b54134d06913975344fb480e73
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 13:58:51 +0100

    refactor(backup.enable): rename order funnel

--- client/app/dedicatedCloud/datacenter/backup/enableAgora/dedicatedCloud-datacenter-backup-enableAgora.routes.js
+++
@@ -1,9 +0,0 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.dedicatedClouds.datacenter.backup.enableAgora', {
-    url: '/enableAgora',
-    templateUrl: 'dedicatedCloud/datacenter/backup/enableAgora/dedicatedCloud-datacenter-backup-enableAgora.html',
-    controller: 'DedicatedCloudSubDatacenterVeeamBackupEnableAgoraCtrl',
-    controllerAs: '$ctrl',
-    layout: 'modal',
-  });
-});
