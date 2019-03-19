2b6cd792a90aa2a92714d10fec4697731cd04e6e
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 13:28:13 +0100

    refactor(license.enable): reorder parameters alphabetically

--- client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
+++ client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
@@ -3,11 +3,11 @@ angular
   .config(($stateProvider, constants) => {
     if (constants.target === 'US') {
       $stateProvider.state('app.dedicatedClouds.license.enable', {
-        url: '/enable',
-        templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
         controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
         controllerAs: '$ctrl',
         layout: 'modal',
+        templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
+        url: '/enable',
       });
     }
   });
