2ad181fc5560a30a5f100b527c123222f97de870
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 16:31:18 +0100

    refactor(datacenter): reorder parameters alphabetically

--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datastore-datacenter.routes.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datastore-datacenter.routes.js
@@ -2,30 +2,30 @@ angular
   .module('App')
   .config(/* @ngInject */ ($stateProvider) => {
     $stateProvider.state('app.dedicatedClouds.datacenter.datastores', {
-      url: '/datastores',
       reloadOnSearch: false,
+      url: '/datastores',
       views: {
         pccDatacenterView: {
-          templateUrl: 'dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.html',
           controller: 'DedicatedCloudSubDatacentersDatastoreCtrl',
           controllerAs: '$ctrl',
+          templateUrl: 'dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.html',
         },
       },
       translations: { value: ['../../../dedicated/server'], format: 'json' },
     });
 
     $stateProvider.state('app.dedicatedClouds.datacenter.datastores.orderUS', {
+      resolve: {
+        datacenterId: /* @ngInject */ $stateParams => $stateParams.datacenterId,
+        serviceName: /* @ngInject */ $stateParams => $stateParams.productId,
+      },
       url: '/order',
       views: {
         'pccDatacenterView@app.dedicatedClouds.datacenter': {
-          templateUrl: 'dedicatedCloud/datacenter/datastore/orderUS/dedicatedCloud-datacenter-datastore-orderUS.html',
           controller: 'DedicatedCloudDatacentersDatastoreOrderUSCtrl',
           controllerAs: '$ctrl',
+          templateUrl: 'dedicatedCloud/datacenter/datastore/orderUS/dedicatedCloud-datacenter-datastore-orderUS.html',
         },
-      },
-      resolve: {
-        serviceName: /* @ngInject */ $stateParams => $stateParams.productId,
-        datacenterId: /* @ngInject */ $stateParams => $stateParams.datacenterId,
       },
     });
   });
