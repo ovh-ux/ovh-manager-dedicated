3f5464e7c0c325169277f9d9cc52ea34590abd2c
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 13:55:17 +0100

    refactor(backup.enable): rename legacy funnel

--- client/app/dedicatedCloud/datacenter/backup/enableLegacy/dedicatedCloud-datacenter-backup-enableLegacy.controller.js
+++ client/app/dedicatedCloud/datacenter/backup/enableLegacy/dedicatedCloud-datacenter-backup-enableLegacy.controller.js
@@ -4,87 +4,200 @@ angular
     /* @ngInject */
     constructor(
       $q,
-      $stateParams,
       $state,
+      $stateParams,
       $translate,
+      $window,
       Alerter,
       constants,
+      datacenterBackupEnableService,
       DedicatedCloud,
       User,
     ) {
       this.$q = $q;
-      this.$stateParams = $stateParams;
       this.$state = $state;
+      this.$stateParams = $stateParams;
       this.$translate = $translate;
+      this.$window = $window;
       this.Alerter = Alerter;
       this.constants = constants;
+      this.datacenterBackupEnableService = datacenterBackupEnableService;
       this.DedicatedCloud = DedicatedCloud;
       this.User = User;
     }
 
     $onInit() {
-      this.loading = {
-        init: false,
-        enable: false,
+      this.bindings = {
+        availableHosts: {
+          value: undefined,
+          warning: {
+            isDisplayed: undefined,
+            text: undefined,
+          },
+        },
+        availableOffers: {
+          value: undefined,
+          selection: {
+            value: undefined,
+          },
+          warning: {
+            canExist: undefined,
+            isDisplayed: undefined,
+            text: undefined,
+          },
+        },
+        isFetchingInitialData: undefined,
+        labels: {
+          primary: undefined,
+          secondary: undefined,
+        },
+        isWarning: undefined,
       };
-
-      this.datacenter = null;
-      this.hosts = null;
 
       return this.fetchInitialData();
     }
 
     fetchInitialData() {
-      this.loading.init = true;
+      this.bindings.isFetchingInitialData = true;
 
       return this.$q
         .all({
+          currentService: this.DedicatedCloud
+            .getSelected(this.$stateParams.productId, true),
           datacenter: this.DedicatedCloud
             .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId),
+          expressURL: this.User
+            .getUrlOf('express_order'),
           hosts: this.DedicatedCloud
             .getHosts(this.$stateParams.productId, this.$stateParams.datacenterId),
           user: this.User
             .getUser(),
+          veamBackupUrl: this.User
+            .getUrlOf('veeamBackup'),
         })
-        .then(({ datacenter, hosts, user }) => {
+        .then(({
+          currentService,
+          datacenter,
+          expressURL,
+          hosts,
+          user,
+          veamBackupUrl,
+        }) => {
+          this.currentService = currentService;
           this.datacenter = datacenter;
-          this.hosts = hosts;
+          this.expressURL = expressURL;
+          this.updateAvailableHosts(hosts);
           this.user = user;
           const urlBaseToUse = _.get(this.constants.urls, this.user.ovhSubsidiary)
             || this.constants.urls.FR;
           this.veeamPresentationURL = urlBaseToUse.presentations.veeam;
+          this.veamBackupUrl = veamBackupUrl;
         })
-        .finally(() => {
-          this.loading.init = false;
-        });
-    }
-
-    onConfirmBtnClick() {
-      if (!this.hosts.length) {
-        return this.onCancelBtnClick();
-      }
-
-      this.loading.enable = true;
-
-      return this.DedicatedCloud
-        .enableVeeam(this.$stateParams.productId, this.$stateParams.datacenterId)
+        .then(() => (this.user.ovhSubsidiary === 'US'
+          ? this.datacenterBackupEnableService.fetchBackupOffers(this.$stateParams.productId)
+          : null))
+        .then((offers) => {
+          this.updateAvailableOffers(_.get(offers, 'prices'));
+        })
         .then(() => {
-          this.Alerter.success(this.$translate.instant('dedicatedCloud_tab_veeam_enable_success', {
-            t0: this.datacenter.name,
-          }), 'dedicatedCloudDatacenterAlert');
+          this.updateType();
+          this.updatePrimaryLabel();
+          this.updateSecondaryLabel();
         })
         .catch((error) => {
-          this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail', {
-            t0: this.datacenter.name,
-          }), error, 'dedicatedCloudDatacenterAlert');
+          this.Alerter.error(`${this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail')}. ${_.get(error, 'message', '')}`.trim());
         })
         .finally(() => {
-          this.loading.enable = false;
-          this.onCancelBtnClick();
+          this.bindings.isFetchingInitialData = false;
         });
     }
 
-    onCancelBtnClick() {
+    updatePrimaryLabel() {
+      this.bindings.labels.primary = this.bindings.isWarning
+        ? this.$translate.instant('global_OK')
+        : this.$translate.instant('dedicatedCloud_options_activate');
+    }
+
+    updateSecondaryLabel() {
+      this.bindings.labels.secondary = this.bindings.isWarning
+        ? null
+        : this.$translate.instant('wizard_cancel');
+    }
+
+    updateType() {
+      this.bindings.isWarning = this.bindings.availableHosts.warning.isDisplayed
+          || this.bindings.availableOffers.warning.isDisplayed;
+    }
+
+    updateAvailableHosts(availableHosts) {
+      this.bindings.availableHosts.value = availableHosts;
+
+      this.bindings.availableHosts.warning.isDisplayed = !_.isArray(availableHosts)
+        || _.isEmpty(availableHosts);
+
+      this.bindings.availableHosts.warning.text = this.bindings.availableHosts.warning.isDisplayed
+        ? this.$translate.instant('dedicatedCloud_tab_veeam_on_disabled', { name: this.datacenter.name })
+        : '';
+    }
+
+    updateAvailableOffers(availableOffers) {
+      this.bindings.availableOffers.value = availableOffers;
+
+      this.bindings.availableOffers.warning.canExist = this.user.ovhSubsidiary === 'US';
+
+      const isDisplayed = this.bindings.availableOffers.warning.canExist
+        && (!_.isArray(availableOffers)
+          || _.isEmpty(availableOffers));
+      this.bindings.availableOffers.warning.isDisplayed = isDisplayed;
+
+      this.bindings.availableOffers.warning.text = this.bindings.availableOffers.warning.isDisplayed
+        ? this.$translate.instant('dedicatedCloud_datacenter_backup_enable_no_offer', { name: this.datacenter.name })
+        : null;
+
+      this.bindings.availableOffers.selection.isDisplayed = _.isArray(availableOffers)
+        && !_.isEmpty(availableOffers);
+
+      this.bindings.availableOffers.selection.value = 'classic';
+    }
+
+    closeModal() {
       return this.$state.go('^');
+    }
+
+    submit() {
+      if (this.bindings.isWarning) {
+        return this.closeModal();
+      }
+
+      if (
+        this.bindings.availableOffers.selection.isDisplayed
+          && !this.bindings.availableOffers.selection.value
+      ) {
+        return null;
+      }
+
+      const offerType = this.bindings.availableOffers.selection.value || 'legacy';
+
+      const productToOrder = {
+        productId: 'privateCloud',
+        serviceName: this.$stateParams.productId,
+        planCode: 'pcc-option-backup-managed',
+        duration: 'P1M',
+        pricingMode: `pcc-servicepack-${this.currentService.servicePackName}`,
+        quantity: 1,
+        configuration: [{
+          label: 'datacenter_id',
+          value: this.datacenter.datacenterId,
+        }, {
+          label: 'offer_type',
+          value: offerType,
+        }],
+      };
+
+      const orderURL = `${this.expressURL}review?products=${JSURL.stringify([productToOrder])}`;
+
+      this.$window.open(orderURL, '_blank');
+
+      return this.closeModal();
     }
   });
