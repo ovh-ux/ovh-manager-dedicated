76cb698a725f01d88d534ce7710e4088d9b61f14
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 16:29:18 +0100

    style(datacenter): change indentation

--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datastore-datacenter.routes.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datastore-datacenter.routes.js
@@ -1,29 +1,31 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.dedicatedClouds.datacenter.datastores', {
-    url: '/datastores',
-    reloadOnSearch: false,
-    views: {
-      pccDatacenterView: {
-        templateUrl: 'dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.html',
-        controller: 'DedicatedCloudSubDatacentersDatastoreCtrl',
-        controllerAs: '$ctrl',
+angular
+  .module('App')
+  .config(($stateProvider) => {
+    $stateProvider.state('app.dedicatedClouds.datacenter.datastores', {
+      url: '/datastores',
+      reloadOnSearch: false,
+      views: {
+        pccDatacenterView: {
+          templateUrl: 'dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.html',
+          controller: 'DedicatedCloudSubDatacentersDatastoreCtrl',
+          controllerAs: '$ctrl',
+        },
       },
-    },
-    translations: ['../../../dedicated/server'],
-  });
+      translations: ['../../../dedicated/server'],
+    });
 
-  $stateProvider.state('app.dedicatedClouds.datacenter.datastores.orderUS', {
-    url: '/order',
-    views: {
-      'pccDatacenterView@app.dedicatedClouds.datacenter': {
-        templateUrl: 'dedicatedCloud/datacenter/datastore/orderUS/dedicatedCloud-datacenter-datastore-orderUS.html',
-        controller: 'DedicatedCloudDatacentersDatastoreOrderUSCtrl',
-        controllerAs: '$ctrl',
+    $stateProvider.state('app.dedicatedClouds.datacenter.datastores.orderUS', {
+      url: '/order',
+      views: {
+        'pccDatacenterView@app.dedicatedClouds.datacenter': {
+          templateUrl: 'dedicatedCloud/datacenter/datastore/orderUS/dedicatedCloud-datacenter-datastore-orderUS.html',
+          controller: 'DedicatedCloudDatacentersDatastoreOrderUSCtrl',
+          controllerAs: '$ctrl',
+        },
       },
-    },
-    resolve: {
-      serviceName: $stateParams => $stateParams.productId,
-      datacenterId: $stateParams => $stateParams.datacenterId,
-    },
+      resolve: {
+        serviceName: $stateParams => $stateParams.productId,
+        datacenterId: $stateParams => $stateParams.datacenterId,
+      },
+    });
   });
-});
