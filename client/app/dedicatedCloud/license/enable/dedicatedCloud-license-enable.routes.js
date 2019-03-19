19e02d8371b216113b0d8b46cc33bbc60c3b2b5f
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 13:27:25 +0100

    style(license.enable): change indentation

--- client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
+++ client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.routes.js
@@ -1,11 +1,13 @@
-angular.module('App').config(($stateProvider, coreConfigProvider) => {
-  if (coreConfigProvider.getRegion() === 'US') {
-    $stateProvider.state('app.dedicatedClouds.license.enable', {
-      url: '/enable',
-      templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
-      controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
-      controllerAs: '$ctrl',
-      layout: 'modal',
-    });
-  }
-});
+angular
+  .module('App')
+  .config(($stateProvider, coreConfigProvider) => {
+    if (coreConfigProvider.getRegion() === 'US') {
+      $stateProvider.state('app.dedicatedClouds.license.enable', {
+        url: '/enable',
+        templateUrl: 'dedicatedCloud/license/enable/dedicatedCloud-license-enable-us.html',
+        controller: 'DedicatedCloudLicencesSplaEnableUSCtrl',
+        controllerAs: '$ctrl',
+        layout: 'modal',
+      });
+    }
+  });
