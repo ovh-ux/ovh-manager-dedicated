e5133b36b20a102b172fcf3d09722d2afc1e52bc
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 11:04:41 +0100

    style(dedicated-cloud): change indentation

--- client/app/dedicatedCloud/dedicatedCloud.routes.js
+++ client/app/dedicatedCloud/dedicatedCloud.routes.js
@@ -1,27 +1,29 @@
-angular.module('App').config(($stateProvider, $urlServiceProvider) => {
-  $stateProvider.state('app.dedicatedClouds', {
-    url: '/configuration/dedicated_cloud/:productId',
-    views: {
-      '': {
-        templateUrl: 'dedicatedCloud/dedicatedCloud.html',
-        controller: 'DedicatedCloudCtrl',
+angular
+  .module('App')
+  .config(($stateProvider, $urlServiceProvider) => {
+    $stateProvider.state('app.dedicatedClouds', {
+      url: '/configuration/dedicated_cloud/:productId',
+      views: {
+        '': {
+          templateUrl: 'dedicatedCloud/dedicatedCloud.html',
+          controller: 'DedicatedCloudCtrl',
+        },
+        'pccView@app.dedicatedClouds': {
+          templateUrl: 'dedicatedCloud/dashboard/dedicatedCloud-dashboard.html',
+        },
       },
-      'pccView@app.dedicatedClouds': {
-        templateUrl: 'dedicatedCloud/dashboard/dedicatedCloud-dashboard.html',
-      },
-    },
-    reloadOnSearch: false,
-    translations: ['.'],
-  });
+      reloadOnSearch: false,
+      translations: ['.'],
+    });
 
-  // ensure compatibility with links sended by emails
-  // like #/configuration/dedicated_cloud/pcc-123456?action=confirmcancel&token=myToken
-  // make a redirect to the new url of ui route
-  $urlServiceProvider.rules.when('/configuration/dedicated_cloud/:productId?action&token', (match) => {
-    if (match.action === 'confirmcancel') {
-      return `/configuration/dedicated_cloud/${match.productId}/terminate-confirm?token=${match.token}`;
-    }
+    // ensure compatibility with links sended by emails
+    // like #/configuration/dedicated_cloud/pcc-123456?action=confirmcancel&token=myToken
+    // make a redirect to the new url of ui route
+    $urlServiceProvider.rules.when('/configuration/dedicated_cloud/:productId?action&token', (match) => {
+      if (match.action === 'confirmcancel') {
+        return `/configuration/dedicated_cloud/${match.productId}/terminate-confirm?token=${match.token}`;
+      }
 
-    return false;
+      return false;
+    });
   });
-});
