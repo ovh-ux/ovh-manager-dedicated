60ae411c19bfd1c74c3839f93fd5aff5a7923415
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-19 13:58:51 +0100

    refactor(backup.enable): rename order funnel

---
+++ client/app/dedicatedCloud/datacenter/backup/enable/dedicatedCloud-datacenter-backup-enable.routes.js
@@ -0,0 +1,9 @@
+angular.module('App').config(($stateProvider) => {
+  $stateProvider.state('app.dedicatedClouds.datacenter.backup.enable', {
+    url: '/enable',
+    templateUrl: 'dedicatedCloud/datacenter/backup/enable/dedicatedCloud-datacenter-backup-enable.html',
+    controller: 'DedicatedCloudSubDatacenterVeeamBackupEnableCtrl',
+    controllerAs: '$ctrl',
+    layout: 'modal',
+  });
+});
