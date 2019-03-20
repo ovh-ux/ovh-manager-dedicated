b24faa93e3fe6c393ae19d69832e793cdda3e40a
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-20 14:37:19 +0100

    refactor(license): rename legacy order

--- client/app/dedicatedCloud/license/dedicatedCloud-license.controller.js
+++ client/app/dedicatedCloud/license/dedicatedCloud-license.controller.js
@@ -39,7 +39,7 @@ angular.module('App').controller('DedicatedCloudLicencesCtrl', ($rootScope, $sco
     if (!currentService.usesLegacyOrder) {
       $state.go('app.dedicatedClouds.license.enable');
     } else {
-      $scope.setAction('license/enable/legacy/dedicatedCloud-license-enable');
+      $scope.setAction('license/enable/legacy/dedicatedCloud-license-enableLegacy');
     }
   };
 
