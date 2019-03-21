cf819e8f8c1e43a37ca1d1ade3db1400aa226ea2
Frédéric Espiau <7319147+FredericEspiau@users.noreply.github.com>
2019-03-21 14:27:15 +0100

    [Support new order funnel - Part 3] Miscellaneous fixes for Dedicated Datacenter (#1022)
    * style(dedicated-cloud-dedicated): change indentation
    
    * refactor(dedicated-cloud-datacenter): remove unused injection
    
    * refactor(name-edition): return Promise, fix indentation and
    
    stop returning a function that displays a message
    
    * refactor(dedicated-cloud-datacenter): turn into class
    
    * refactor(dedicated-cloud): export alert id to a constant
    
    * feat(name-edition): dialog closes on error and
    
    can select where to write the error message
    
    * fix(dedicated-cloud-datacenter): dialog will close and display error
    
    message in case of error
    
    * style(dedicated-cloud-datacenter-host): change indentation
    
    * fix(dedicated-cloud-datacenter-host): add injection annotations
    
    * style(dedicated-cloud-datacenters-backup-disable): change indent
    
    * style(dedicated-cloud-datacenters-backup-disable): remove comments
    
    * style(dedicated-cloud-datacenters-backup-disable): move  function
    
    * refactor(dedicated-cloud-datacenters-backup-disable): move datacenter
    
     info proxy data fetching into own function
    
    * refactor(dedicated-cloud-datacenters-backup-disable): return Promise
    
    * style(backup): change indentation
    
    * style(backup): reorder injections alphabetically
    
    * refactor(backup): turn into a class
    
    * style(backup): change indentation
    
    * fix(backup): add missing annotation
    
    * style(backup.enable): change indentation
    
    * fix(backup.enable): add missing annotation
    
    * style(backup.enable): change indentation
    
    * style(backup.enable): remove comments
    
    * style(backup.enable): move function order
    
    * refactor(backup.enable): move initialization data to
    
    * refactor(backup.enable): create function to fetch initial data
    
    * fix(backup.enable): return Promise
    
    * refactor(backup.enable-agora): create method to fetch initial data
    
    * refactor(backup.enable-agora): return Promise
    
    * style(datastore): change indentation
    
    * refactor(datastore): turn into class
    
    * refactor(dedicated-cloud.datacenter): add missing annotations
    
    * style(host): change indentation
    
    * refactor(host): remove unused injection
    
    * refactor(host): turn into class
    
    * refactor(host): move datacenter info proxy fetching into own method
    
    * style(host.order-us): change indentation
    
    * style(host.order-us): reorder injection alphabetically
    
    * style(host.order-us): change function order
    
    * refactor(host.order-us): create method to fetch initial data
    
    * refactor(host.order-us): move data initialization to  method
    
    * fix(host.order-us): add missing annotation
    
    * fix(datacenter): increase margin to match other pages
    
    * fix(datacenter): add margin to button to match other pages
    
    * refactor(dedicated-cloud): rename functions
    
    remove reference to previous system to use the API
    
    * feat(name-edition): add option to display success message
    
    * feat(dedicated-cloud): display success message when changing name
    
    * feat(datacenter): display success message
    
    when changing name or description
    
    * fix(veeam): remove unwanted tags in translation

--- client/app/dedicatedCloud/datacenter/backup/enableAgora/dedicatedCloud-datacenter-backup-enableAgora.controller.js
+++ client/app/dedicatedCloud/datacenter/backup/enableAgora/dedicatedCloud-datacenter-backup-enableAgora.controller.js
@@ -1,90 +1,116 @@
-angular.module('App').controller('DedicatedCloudSubDatacenterVeeamBackupEnableAgoraCtrl', class {
-  constructor($q, $state, $stateParams, $translate, $window,
-    Alerter, DedicatedCloud, OvhHttp, User) {
-    this.$q = $q;
-    this.$state = $state;
-    this.$stateParams = $stateParams;
-    this.$translate = $translate;
-    this.$window = $window;
-    this.Alerter = Alerter;
-    this.DedicatedCloud = DedicatedCloud;
-    this.OvhHttp = OvhHttp;
-    this.User = User;
-
-    this.loading = {
-      init: false,
-    };
-
-    this.model = {
-      version: 'classic',
-    };
-  }
-
-  closeModal() {
-    this.$state.go('^');
-  }
-
-  onBackupEnableFormSubmit() {
-    if (!this.hosts || !this.hosts.length || !this.offer) {
-      return this.closeModal();
+angular
+  .module('App')
+  .controller('DedicatedCloudSubDatacenterVeeamBackupEnableAgoraCtrl', class {
+    /* @ngInject */
+    constructor(
+      $q,
+      $state,
+      $stateParams,
+      $translate,
+      $window,
+      Alerter,
+      DedicatedCloud,
+      OvhHttp,
+      User,
+    ) {
+      this.$q = $q;
+      this.$state = $state;
+      this.$stateParams = $stateParams;
+      this.$translate = $translate;
+      this.$window = $window;
+      this.Alerter = Alerter;
+      this.DedicatedCloud = DedicatedCloud;
+      this.OvhHttp = OvhHttp;
+      this.User = User;
     }
 
-    const productToOrder = {
-      productId: 'privateCloud',
-      serviceName: this.$stateParams.productId,
-      planCode: this.offer.planCode,
-      duration: 'P1M',
-      pricingMode: 'pcc-servicepack-nsx',
-      quantity: 1,
-      configuration: [{
-        label: 'datacenter_id',
-        value: this.datacenter.datacenterId,
-      }, {
-        label: 'offer_type',
-        value: this.model.version,
-      }],
-    };
+    $onInit() {
+      this.loading = {
+        init: false,
+      };
 
-    return this.User.getUrlOf('express_order')
-      .then((url) => {
-        this.$window.open(`${url}review?products=${JSURL.stringify([productToOrder])}`, '_blank');
-      })
-      .catch((error) => {
-        this.Alerter.error([this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail'), _.get(error, 'data.message')].join(' '));
-      })
-      .finally(() => this.closeModal());
-  }
+      this.model = {
+        version: 'classic',
+      };
 
-  getBackupOffer() {
-    return this.OvhHttp.get('/order/cartServiceOption/privateCloud/{serviceName}', {
-      rootPath: 'apiv6',
-      urlParams: {
+      return this.fetchInitialData();
+    }
+
+    closeModal() {
+      return this.$state.go('^');
+    }
+
+    fetchInitialData() {
+      this.loading.init = true;
+
+      return this.$q
+        .all({
+          datacenter: this.DedicatedCloud
+            .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId),
+          hosts: this.DedicatedCloud
+            .getHosts(this.$stateParams.productId, this.$stateParams.datacenterId),
+          offer: this.getBackupOffer(),
+          veamBackupUrl: this.User.getUrlOf('veeamBackup'),
+        })
+        .then(({
+          datacenter,
+          hosts,
+          offer,
+          veamBackupUrl,
+        }) => {
+          this.datacenter = datacenter;
+          this.hosts = hosts;
+          this.offer = offer;
+          this.veamBackupUrl = veamBackupUrl;
+        })
+        .catch((error) => {
+          this.Alerter.error([this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail'), _.get(error, 'data.message')].join(' '));
+        })
+        .finally(() => {
+          this.loading.init = false;
+        });
+    }
+
+    getBackupOffer() {
+      return this.OvhHttp
+        .get('/order/cartServiceOption/privateCloud/{serviceName}', {
+          rootPath: 'apiv6',
+          urlParams: {
+            serviceName: this.$stateParams.productId,
+          },
+        })
+        .then(offers => _.find(offers, { family: 'backup' }));
+    }
+
+    onBackupEnableFormSubmit() {
+      if (!this.hosts || !this.hosts.length || !this.offer) {
+        return this.closeModal();
+      }
+
+      const productToOrder = {
+        productId: 'privateCloud',
         serviceName: this.$stateParams.productId,
-      },
-    }).then(offers => _.find(offers, { family: 'backup' }));
-  }
+        planCode: this.offer.planCode,
+        duration: 'P1M',
+        pricingMode: 'pcc-servicepack-nsx',
+        quantity: 1,
+        configuration: [{
+          label: 'datacenter_id',
+          value: this.datacenter.datacenterId,
+        }, {
+          label: 'offer_type',
+          value: this.model.version,
+        }],
+      };
 
-  $onInit() {
-    this.loading.init = true;
-
-    return this.$q.all({
-      datacenter: this.DedicatedCloud
-        .getDatacenterInfoProxy(this.$stateParams.productId, this.$stateParams.datacenterId),
-      hosts: this.DedicatedCloud
-        .getHostsLexi(this.$stateParams.productId, this.$stateParams.datacenterId),
-      offer: this.getBackupOffer(),
-      veamBackupUrl: this.User.getUrlOf('veeamBackup'),
-    }).then(({
-      datacenter, hosts, offer, veamBackupUrl,
-    }) => {
-      this.datacenter = datacenter;
-      this.hosts = hosts;
-      this.offer = offer;
-      this.veamBackupUrl = veamBackupUrl;
-    }).catch((error) => {
-      this.Alerter.error([this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail'), _.get(error, 'data.message')].join(' '));
-    }).finally(() => {
-      this.loading.init = false;
-    });
-  }
-});
+      return this.User
+        .getUrlOf('express_order')
+        .then((url) => {
+          this.$window.open(`${url}review?products=${JSURL.stringify([productToOrder])}`, '_blank');
+        })
+        .catch((error) => {
+          this.Alerter.error([this.$translate.instant('dedicatedCloud_tab_veeam_enable_fail'), _.get(error, 'data.message')].join(' '));
+        })
+        .finally(() => this.closeModal());
+    }
+  });
--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
@@ -1,47 +1,83 @@
-angular.module('App').controller('DedicatedCloudSubDatacentersDatastoreCtrl', function ($q, $scope, $state, $stateParams, constants, DedicatedCloud) {
-  $scope.constants = constants;
-  const noConsumptionResponse = new RegExp('no consumption', 'i');
-
-  this.loadDatastores = ({ offset, pageSize }) => DedicatedCloud.getDatastores(
-    $stateParams.productId,
-    $stateParams.datacenterId,
-    pageSize,
-    offset - 1,
-  ).then((result) => {
-    const datastores = _.get(result, 'list.results');
-    return $q.all(datastores.map((dc) => {
-      if (dc.billing === 'HOURLY') {
-        return DedicatedCloud.getDatastoreHourlyConsumption(
-          $stateParams.productId,
-          $stateParams.datacenterId,
-          dc.id,
-        ).then((response) => {
-          _.set(dc, 'consumption', _.get(response, 'consumption.value'));
-          _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
-          return dc;
-        }).catch((err) => {
-          if (noConsumptionResponse.test(err.message)) {
-            _.set(dc, 'consumption', 0);
-          } else {
-            _.set(dc, 'consumption', null);
-          }
-          return dc;
-        });
-      }
-      return $q.when(dc);
-    })).then(data => ({
-      data,
-      meta: {
-        totalCount: result.count,
-      },
-    }));
-  });
-
-  this.orderDatastore = (datacenter) => {
-    if (constants.target === 'US') {
-      $state.go('app.dedicatedClouds.datacenter.datastores.orderUS');
-    } else {
-      $scope.setAction('datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order', datacenter.model, true);
+angular
+  .module('App')
+  .controller('DedicatedCloudSubDatacentersDatastoreCtrl', class {
+    /* @ngInject */
+    constructor(
+      $q,
+      $scope,
+      $state,
+      $stateParams,
+      constants,
+      DedicatedCloud,
+    ) {
+      this.$q = $q;
+      this.$scope = $scope;
+      this.$state = $state;
+      this.$stateParams = $stateParams;
+      this.constants = constants;
+      this.DedicatedCloud = DedicatedCloud;
     }
-  };
-});
+
+    $onInit() {
+      this.$scope.constants = this.constants;
+      this.noConsumptionResponse = new RegExp('no consumption', 'i');
+
+      this.$scope.loadDatastores = ({ offset, pageSize }) => this
+        .loadDatastores({ offset, pageSize });
+      this.$scope.orderDatastore = datacenter => this.orderDatastore(datacenter);
+    }
+
+    loadDatastores({ offset, pageSize }) {
+      return this.DedicatedCloud
+        .getDatastores(
+          this.$stateParams.productId,
+          this.$stateParams.datacenterId,
+          pageSize, offset - 1,
+        )
+        .then((result) => {
+          const datastores = _.get(result, 'list.results');
+
+          return this.$q
+            .all(datastores
+              .map((dc) => {
+                if (dc.billing === 'HOURLY') {
+                  return this.DedicatedCloud
+                    .getDatastoreHourlyConsumption(
+                      this.$stateParams.productId,
+                      this.$stateParams.datacenterId,
+                      dc.id,
+                    )
+                    .then((response) => {
+                      _.set(dc, 'consumption', _.get(response, 'consumption.value'));
+                      _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
+                      return dc;
+                    }).catch((err) => {
+                      if (this.noConsumptionResponse.test(err.message)) {
+                        _.set(dc, 'consumption', 0);
+                      } else {
+                        _.set(dc, 'consumption', null);
+                      }
+
+                      return dc;
+                    });
+                }
+
+                return this.$q.when(dc);
+              }))
+            .then(data => ({
+              data,
+              meta: {
+                totalCount: result.count,
+              },
+            }));
+        });
+    }
+
+    orderDatastore(datacenter) {
+      if (this.constants.target === 'US') {
+        this.$state.go('app.dedicatedClouds.datacenter.datastores.orderUS');
+      } else {
+        this.$scope.setAction('datacenter/datastore/order/dedicatedCloud-datacenter-datastore-order', datacenter.model, true);
+      }
+    }
+  });
