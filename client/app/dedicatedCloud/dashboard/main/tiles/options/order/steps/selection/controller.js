f88123fc608a291927ee3af287b382bad598b54f
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-03-22 17:47:37 +0100

    fix: merge errors

--- client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/controller.js
+++ client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/controller.js
@@ -22,28 +22,29 @@ export default class {
       { name: this.currentService.servicePackName },
     );
 
-    this.orderableServicePacks = this.orderableServicePacks.map((servicePack) => {
-      const matchingServicePack = _.find(this.servicePacksWithPrices, { name: servicePack.name });
-      const priceAsNumber = matchingServicePack.price.value - currentServicePack.price.value;
+    this.orderableServicePacks = this.orderableServicePacks
+      .map((servicePack) => {
+        const matchingServicePack = _.find(this.servicePacksWithPrices, { name: servicePack.name });
+        const priceAsNumber = matchingServicePack.price.value - currentServicePack.price.value;
 
-      const priceAsString = new Intl
-        .NumberFormat(
-          'fr', // can't change as the API is not ISO compliant
-          {
-            style: 'currency',
-            currency: currentServicePack.price.currencyCode,
-            minimumFractionDigits: 2,
-          },
-        )
-        .format(priceAsNumber);
+        const priceAsString = new Intl
+          .NumberFormat(
+            'fr', // can't change as the API is not ISO compliant
+            {
+              style: 'currency',
+              currency: currentServicePack.price.currencyCode,
+              minimumFractionDigits: 2,
+            },
+          )
+          .format(priceAsNumber);
 
-      const price = priceAsNumber > 0 ? `+${priceAsString}` : priceAsString;
+        const price = priceAsNumber > 0 ? `+${priceAsString}` : priceAsString;
 
-      return {
-        ...servicePack,
-        price,
-      };
-    });
+        return {
+          ...servicePack,
+          price,
+        };
+      });
   }
 
   goToNextStep() {
@@ -66,29 +67,38 @@ export default class {
         serviceName: `${this.currentService.serviceName}/servicepack`,
         planCode: `pcc-servicepack-${this.servicePackToOrder.name}`,
         quantity: 1,
-        autoPayWithPreferredPaymentMethod: false,
+        autoPayWithPreferredPaymentMethod: this.hasDefaultMeansOfPayment,
       }).$promise
       .then(({ order }) => {
-        this.orderIsInProgress = false;
         this.orderingURL = order.url;
-        return this.servicePackService.savePendingOrder(
-          this.currentService.serviceName,
-          {
+
+        return this.servicePackService
+          .savePendingOrder(this.currentService.serviceName, {
             activationType: this.activationType,
             id: order.orderId,
             url: order.url,
             orderedServicePackName: this.servicePackToOrder.name,
-          },
-        );
+          })
+          .then(() => {
+            if (this.hasDefaultMeansOfPayment) {
+              if (this.activationType === 'basic') {
+                return this.stepper.exit();
+              }
+
+              return this.goToNextStep();
+            }
+
+            return null;
+          });
       })
       .catch(error => this.stepper
         .exit()
-        .then(() => this.Alerter.alertFromSWS(
-          this.$translate.instant('dedicatedCloudDashboardTilesOptionsOrderSelection_order_failure'),
-          {
-            message: error.data.message,
-            type: 'ERROR',
-          },
-        )));
+        .then(() => this.Alerter.alertFromSWS(this.$translate.instant('dedicatedCloudDashboardTilesOptionsOrderSelection_order_failure'), {
+          message: error.data.message,
+          type: 'ERROR',
+        }, 'dedicatedCloud_alert')))
+      .finally(() => {
+        this.orderIsInProgress = false;
+      });
   }
 }
