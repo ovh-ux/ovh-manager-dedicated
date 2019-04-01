555c9f988103f495e67e0e9fc2c5c1a0d03e162b
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-04-01 10:05:03 +0200

    fix(options.order): set order at final order step

--- client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/constants.js
+++ client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/constants.js
@@ -1,9 +1,11 @@
+export const BASIC_ACTIVATION_TYPE = 'basic';
 export const COMPONENT_NAME = 'dedicatedCloudServicePackSelection';
 export const MODULE_NAME = 'dedicatedCloudServicePackSelection';
 export const SERVICE_NAME = 'selectionService';
 export const STEP_NAME = 'selection';
 
 export default {
+  BASIC_ACTIVATION_TYPE,
   COMPONENT_NAME,
   MODULE_NAME,
   SERVICE_NAME,
--- client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/summary/controller.js
+++ client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/summary/controller.js
@@ -1,25 +1,65 @@
+import { PACK_ACTIVATION_PREFERENCE_KEY } from './constants';
+
 export default class DedicatedCloudservicePackSmsActivation {
   /* @ngInject */
   constructor(
     $q,
     $state,
+    OvhApiOrder,
     ACTIVATION_STATUS,
   ) {
     this.$q = $q;
     this.$state = $state;
+    this.OvhApiOrder = OvhApiOrder;
     this.ACTIVATION_STATUS = ACTIVATION_STATUS;
   }
 
   $onInit() {
-    if (this.orderedServicePack == null) {
+    this.currentService = this.stepper.memorizedStateParams.currentService;
+    this.servicePackToOrder = this.stepper.memorizedStateParams.servicePackToOrder;
+
+    if (this.servicePackToOrder == null) {
       return this.stepper.exit();
     }
 
-    return this.ovhUserPref.assign('DEDICATED_CLOUD_SERVICE_PACK_ACTIVATION', {
-      certification: {
-        orderUrl: this.orderURL,
-        status: this.ACTIVATION_STATUS.pendingActivation,
-      },
-    });
+    return this.placeOrder();
+  }
+
+  placeOrder() {
+    this.orderIsInProgress = true;
+
+    return this.OvhApiOrder.Upgrade().PrivateCloud().v6()
+      .post({
+        serviceName: `${this.currentService.name}/servicepack`,
+        planCode: `pcc-servicepack-${this.servicePackToOrder.name}`,
+        quantity: 1,
+        autoPayWithPreferredPaymentMethod: this.hasDefaultMeansOfPayment,
+      }).$promise
+      .then(({ order }) => {
+        this.orderURL = order.url;
+
+        return this.servicePackService
+          .savePendingOrder(this.currentService.name, {
+            activationType: this.activationType,
+            id: order.orderId,
+            url: order.url,
+            orderedServicePackName: this.servicePackToOrder.name,
+          })
+          .then(() => this.ovhUserPref.assign(PACK_ACTIVATION_PREFERENCE_KEY, {
+            certification: {
+              orderUrl: order.url,
+              status: this.ACTIVATION_STATUS.pendingActivation,
+            },
+          }));
+      })
+      .catch(error => this.stepper
+        .exit()
+        .then(() => this.Alerter.alertFromSWS(this.$translate.instant('dedicatedCloud_servicePack_summary_stepper_order_failed'), {
+          message: error.data.message,
+          type: 'ERROR',
+        }, 'dedicatedCloud_alert')))
+      .finally(() => {
+        this.orderIsInProgress = false;
+      });
   }
 }
