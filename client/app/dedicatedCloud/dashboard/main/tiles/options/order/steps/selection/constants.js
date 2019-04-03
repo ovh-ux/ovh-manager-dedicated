272b076df0a635f9ddf01362fda27fce226c5e78
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-04-03 16:28:19 +0200

    fix(dashboard.options): add confirmation modal for options order

--- client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/constants.js
+++ client/app/dedicatedCloud/dashboard/main/tiles/options/order/steps/selection/constants.js
@@ -1,5 +1,7 @@
 export const BASIC_ACTIVATION_TYPE = 'basic';
 export const COMPONENT_NAME = 'dedicatedCloudServicePackSelection';
+export const MODAL_CONTROLLER_NAME = 'dedicatedCloudOptionsConfirmCtrl';
+export const MODAL_TEMPLATE_URL = 'dedicatedCloudOptionsConfirmTemplate.html';
 export const MODULE_NAME = 'dedicatedCloudServicePackSelection';
 export const SERVICE_NAME = 'selectionService';
 export const STEP_NAME = 'selection';
@@ -7,6 +9,8 @@ export const STEP_NAME = 'selection';
 export default {
   BASIC_ACTIVATION_TYPE,
   COMPONENT_NAME,
+  MODAL_CONTROLLER_NAME,
+  MODAL_TEMPLATE_URL,
   MODULE_NAME,
   SERVICE_NAME,
   STEP_NAME,
