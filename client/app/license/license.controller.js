766206685d0a91a5c990c086e89ebdb45991c038
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-04-09 10:48:56 +0200

    refactor(licence): convert to Class

--- client/app/license/license.controller.js
+++ client/app/license/license.controller.js
@@ -1,21 +1,26 @@
 angular
   .module('Module.license')
-  .controller('LicenseCtrl', [
-    '$scope',
-    '$state',
-    'License',
-    '$timeout',
-    'constants',
-    'BILLING_URLS',
-    ($scope,
+  .controller('LicenseCtrl', class {
+    constructor(
+      $scope,
       $state,
       License,
       $timeout,
       constants,
-      billingUrls) => {
-      $scope.licencesTableLoading = false;
-      $scope.licenses = null;
-      $scope.licenseTypes = {
+      billingUrl,
+    ) {
+      this.$scope = $scope;
+      this.$state = $state;
+      this.License = License;
+      this.$timeout = $timeout;
+      this.constants = constants;
+      this.billingUrl = billingUrl;
+    }
+
+    $onInit() {
+      this.$scope.licencesTableLoading = false;
+      this.$scope.licenses = null;
+      this.$scope.licenseTypes = {
         CPANEL: 'CPANEL',
         DIRECTADMIN: 'DIRECTADMIN',
         PLESK: 'PLESK',
@@ -24,111 +29,122 @@ angular
         VIRTUOZZO: 'VIRTUOZZO',
         WINDOWS: 'WINDOWS',
       };
-      $scope.filterType = null;
-      $scope.$state = $state;
+      this.$scope.filterType = null;
+      this.$scope.$state = this.$state;
 
-      /**
+      this.$scope.$on('$locationChangeStart', () => this.$scope.resetAction());
+
+      this.resetAction();
+
+      this.$scope.resetAction = () => this.resetAction();
+      this.$scope.search = type => this.search(type);
+      this.$scope.setAction = (action, data) => this.setAction(action, data);
+      this.$scope.loadDatagridLicences = ({ offset, pageSize }) => this
+        .loadDatagridLicences({ offset, pageSize });
+      this.$scope.loadLicenses = ({ offset, pageSize }) => this.loadLicenses({ offset, pageSize });
+      this.$scope.getLicenseId = license => this.getLicenseId(license);
+    }
+
+    resetAction() {
+      return this.setAction(false);
+    }
+
+    /**
      * Search
      * @param  {String} type
      * @return {Void}
      */
-      $scope.search = (type) => {
-        $scope.filterType = type;
-        $scope.$broadcast('paginationServerSide.loadPage', 1, 'licensesPagination');
-      };
+    search(type) {
+      this.$scope.filterType = type;
+      this.$scope.$broadcast('paginationServerSide.loadPage', 1, 'licensesPagination');
+    }
 
-      $scope.resetAction = () => $scope.setAction(false);
+    setAction(action, data) {
+      if (action) {
+        this.$scope.currentAction = action;
+        this.$scope.currentActionData = data;
+        this.$scope.stepPath = `license/${this.$scope.currentAction}.html`;
+        $('#currentAction').modal({
+          keyboard: true,
+          backdrop: 'static',
+        });
+      } else {
+        $('#currentAction').modal('hide');
+        this.$scope.currentActionData = null;
+        this.$timeout(() => {
+          this.$scope.stepPath = '';
+        }, 300);
+      }
+    }
 
-      $scope.$on('$locationChangeStart', () => $scope.resetAction());
-
-      $scope.setAction = (action, data) => {
-        if (action) {
-          $scope.currentAction = action;
-          $scope.currentActionData = data;
-          $scope.stepPath = `license/${$scope.currentAction}.html`;
-          $('#currentAction').modal({
-            keyboard: true,
-            backdrop: 'static',
-          });
-        } else {
-          $('#currentAction').modal('hide');
-          $scope.currentActionData = null;
-          $timeout(() => {
-            $scope.stepPath = '';
-          }, 300);
-        }
-      };
-
-      $scope.loadDatagridLicences = ({ offset, pageSize }) => $scope
-        .loadLicenses(pageSize, offset - 1)
+    loadDatagridLicences({ offset, pageSize }) {
+      return this.loadLicenses(pageSize, offset - 1)
         .then(licenses => ({
           data: _.get(licenses, 'list.results'),
           meta: {
             totalCount: licenses.count,
           },
         }));
+    }
 
-      /**
+    /**
      * Load licenses.
      * @param  {Number} count
      * @param  {Number} offset
      * @return {Promise}
      */
-      $scope.loadLicenses = (count, offset) => {
-        $scope.licencesTableLoading = true;
-        return License.get('', {
-          params: {
-            filterType: $scope.filterType === 'ALL_TYPE' ? undefined : $scope.filterType,
-            count,
-            offset,
-          },
-        }).then((licenses) => {
-          if (_.chain(licenses).get('availableTypes').isArray().value()) {
-            licenses.availableTypes.push('ALL_TYPE');
-          }
-          angular.forEach(licenses.list.results, (value, idx) => {
-            if (value.expiration !== null) {
+    loadLicenses(count, offset) {
+      this.$scope.licencesTableLoading = true;
+      return this.License.get('', {
+        params: {
+          filterType: this.$scope.filterType === 'ALL_TYPE' ? undefined : this.$scope.filterType,
+          count,
+          offset,
+        },
+      }).then((licenses) => {
+        if (_.chain(licenses).get('availableTypes').isArray().value()) {
+          licenses.availableTypes.push('ALL_TYPE');
+        }
+        angular.forEach(licenses.list.results, (value, idx) => {
+          if (value.expiration !== null) {
             licenses.list.results[idx].isExpired = moment() // eslint-disable-line
-                .isAfter(moment(value.expiration, 'YYYY-MM-DDTHH:mm:ss.SSSZZ'));
+              .isAfter(moment(value.expiration, 'YYYY-MM-DDTHH:mm:ss.SSSZZ'));
             licenses.list.results[idx].expireSoon = moment() // eslint-disable-line
-                .add(1, 'months')
-                .isAfter(moment(value.expiration, 'YYYY-MM-DDTHH:mm:ss.SSSZZ'));
-            }
-          });
-          $scope.licenses = licenses;
-          return licenses;
-        }).finally(() => {
-          $scope.licencesTableLoading = false;
+              .add(1, 'months')
+              .isAfter(moment(value.expiration, 'YYYY-MM-DDTHH:mm:ss.SSSZZ'));
+          }
         });
-      };
+        this.$scope.licenses = licenses;
+        return licenses;
+      }).finally(() => {
+        this.$scope.licencesTableLoading = false;
+      });
+    }
 
-      /**
+    /**
      * Get license Id.
      * @param  {Object} license
      * @return {Number}
      */
-      $scope.getLicenseId = (license) => {
-        if (license.type === $scope.licenseTypes.SPLA) {
-          return `SPLA-${license.id}@${license.serverServiceName}`;
-        }
-        return license.id;
-      };
+    getLicenseId(license) {
+      if (license.type === this.$scope.licenseTypes.SPLA) {
+        return `SPLA-${license.id}@${license.serverServiceName}`;
+      }
+      return license.id;
+    }
 
-      /**
+    /**
      * Get renew URL.
      * @return {String}
      */
-      $scope.getRenewUrl = () => {
-        if (!$scope.user) {
-          return constants.renew.replace('{serviceName}', '');
-        }
-        const renewUrl = billingUrls.renew[$scope.user.ovhSubsidiary];
-        if (!renewUrl) {
-          return constants.renew.replace('{serviceName}', '');
-        }
-        return renewUrl.replace('{serviceName}', '');
-      };
-
-      $scope.resetAction();
-    },
-  ]);
+    getRenewUrl() {
+      if (!this.$scope.user) {
+        return this.constants.renew.replace('{serviceName}', '');
+      }
+      const renewUrl = this.billingUrls.renew[this.$scope.user.ovhSubsidiary];
+      if (!renewUrl) {
+        return this.constants.renew.replace('{serviceName}', '');
+      }
+      return renewUrl.replace('{serviceName}', '');
+    }
+  });
