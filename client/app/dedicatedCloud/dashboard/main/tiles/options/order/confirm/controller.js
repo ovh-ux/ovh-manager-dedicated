30cb69e46304599630e65b0277c5c6b3bb507378
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-04-10 11:19:42 +0200

    fix(order.confirm): handle all price cases (negative, 0 and 1)

--- client/app/dedicatedCloud/dashboard/main/tiles/options/order/confirm/controller.js
+++ client/app/dedicatedCloud/dashboard/main/tiles/options/order/confirm/controller.js
@@ -1,13 +1,42 @@
 export default class {
   constructor(
+    $translate,
     $uibModalInstance,
     optionName,
     price,
     priceAsNumber,
   ) {
+    this.$translate = $translate;
     this.$uibModalInstance = $uibModalInstance;
     this.optionName = optionName;
     this.price = price;
     this.priceAsNumber = priceAsNumber;
+  }
+
+  $onInit() {
+    this.bindings = {
+      text: this.chooseText(),
+    };
+  }
+
+  chooseText() {
+    const translationsValues = {
+      optionName: `<strong>${this.optionName}</strong>`,
+      price: `<strong>${this.price}</strong>`,
+    };
+
+    if (this.priceAsNumber === 0) {
+      return this.$translate.instant('confirm_order_option_question_0', translationsValues);
+    }
+
+    if (this.priceAsNumber < 0) {
+      return this.$translate.instant('confirm_order_option_question_deducting', translationsValues);
+    }
+
+    if (this.priceAsNumber <= 1) {
+      return this.$translate.instant('confirm_order_option_question_adding_1', translationsValues);
+    }
+
+    return this.$translate.instant('confirm_order_option_question_adding_many', translationsValues);
   }
 }
