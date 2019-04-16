8b00dfd2708e4de2e3ec1e16089ed280629cc7bc
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-04-16 14:27:47 +0200

    refactor(license.enable): change indentation && return Promises

--- client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.controller.js
+++ client/app/dedicatedCloud/license/enable/dedicatedCloud-license-enable.controller.js
@@ -1,70 +1,87 @@
-angular.module('App').controller('DedicatedCloudLicencesSplaEnableCtrl', class {
-  constructor($state, $stateParams, $translate, Alerter, OvhHttp, User) {
-    this.$state = $state;
-    this.$translate = $translate;
-    this.OvhHttp = OvhHttp;
-    this.Alerter = Alerter;
-    this.User = User;
-    this.serviceName = $stateParams.productId;
-    this.selectedOffer = null;
-  }
-
-  $onInit() {
-    this.loading = true;
-    return this.User.getUrlOf('express_order').then((url) => {
-      this.expressOrderUrl = url;
-    }).catch(() => {
-      this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_licences_active_spla_load_fail'));
-      this.$state.go('^');
-    }).finally(() => {
-      this.loading = false;
-    });
-  }
-
-  fetchOffers() {
-    return this.OvhHttp.get('/order/cartServiceOption/privateCloud/{serviceName}', {
-      rootPath: 'apiv6',
-      urlParams: {
-        serviceName: this.serviceName,
-      },
-    }).then(offers => _.filter(offers, offer => _.startsWith(offer.planCode, 'pcc-option-windows')));
-  }
-
-  fetchDatagridOffers() {
-    return this.fetchOffers().then((offers) => {
-      this.selectedOffer = _.first(offers);
-      return {
-        data: offers,
-        meta: {
-          totalCount: _.size(offers),
-        },
-      };
-    });
-  }
-
-  onOrderClick() {
-    this.$state.go('^');
-    return true;
-  }
-
-  getOrderUrl() {
-    if (!this.selectedOffer) {
-      return null;
+angular
+  .module('App')
+  .controller('DedicatedCloudLicencesSplaEnableCtrl', class {
+    constructor(
+      $state,
+      $stateParams,
+      $translate,
+      Alerter,
+      OvhHttp,
+      User,
+    ) {
+      this.$state = $state;
+      this.$translate = $translate;
+      this.OvhHttp = OvhHttp;
+      this.Alerter = Alerter;
+      this.User = User;
+      this.serviceName = $stateParams.productId;
+      this.selectedOffer = null;
     }
 
-    const price = _.first(this.selectedOffer.prices);
+    $onInit() {
+      this.loading = true;
 
-    return `${this.expressOrderUrl}review?products=${JSURL.stringify([{
-      productId: 'privateCloud',
-      serviceName: this.serviceName,
-      planCode: this.selectedOffer.planCode,
-      duration: price.duration,
-      pricingMode: price.pricingMode,
-      quantity: 1,
-    }])}`;
-  }
+      return this.User
+        .getUrlOf('express_order')
+        .then((url) => {
+          this.expressOrderUrl = url;
+        })
+        .catch(() => {
+          this.Alerter.error(this.$translate.instant('dedicatedCloud_tab_licences_active_spla_load_fail'));
+          this.$state.go('^');
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    }
 
-  onCancelBtnClick() {
-    this.$state.go('^');
-  }
-});
+    fetchOffers() {
+      return this.OvhHttp
+        .get('/order/cartServiceOption/privateCloud/{serviceName}', {
+          rootPath: 'apiv6',
+          urlParams: {
+            serviceName: this.serviceName,
+          },
+        })
+        .then(offers => _.filter(offers, { offer: 'pcc-option-windows' }));
+    }
+
+    fetchDatagridOffers() {
+      return this
+        .fetchOffers()
+        .then((offers) => {
+          this.selectedOffer = _.first(offers);
+          return {
+            data: offers,
+            meta: {
+              totalCount: _.size(offers),
+            },
+          };
+        });
+    }
+
+    onOrderClick() {
+      return this.$state.go('^');
+    }
+
+    getOrderUrl() {
+      if (!this.selectedOffer) {
+        return null;
+      }
+
+      const price = _.first(this.selectedOffer.prices);
+
+      return `${this.expressOrderUrl}review?products=${JSURL.stringify([{
+        productId: 'privateCloud',
+        serviceName: this.serviceName,
+        planCode: this.selectedOffer.planCode,
+        duration: price.duration,
+        pricingMode: price.pricingMode,
+        quantity: 1,
+      }])}`;
+    }
+
+    onCancelBtnClick() {
+      return this.$state.go('^');
+    }
+  });
