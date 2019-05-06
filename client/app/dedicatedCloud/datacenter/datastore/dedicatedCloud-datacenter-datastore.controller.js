743de1ab0ff8dd9f4a0490d8e4434d6db7c93708
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-05-06 11:12:44 +0200

    refactor(datacenter.datastore): move logic to own function

--- client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
+++ client/app/dedicatedCloud/datacenter/datastore/dedicatedCloud-datacenter-datastore.controller.js
@@ -36,6 +36,36 @@ angular
       this.$scope.orderDatastore = datacenter => this.orderDatastore(datacenter);
     }
 
+    fetchLegacyConsumption(datastores) {
+      return this.$q
+        .all(datastores
+          .map((dc) => {
+            if (dc.billing === this.RESOURCE_BILLING_TYPES.hourly) {
+              return this.dedicatedCloudDataCenterDatastoreService
+                .fetchLegacyHourlyConsumption(
+                  this.$stateParams.productId,
+                  this.$stateParams.datacenterId,
+                  dc.id,
+                )
+                .then((response) => {
+                  _.set(dc, 'consumption', _.get(response, 'consumption.value'));
+                  _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
+                  return dc;
+                }).catch((err) => {
+                  if (this.noConsumptionResponse.test(err.message)) {
+                    _.set(dc, 'consumption', 0);
+                  } else {
+                    _.set(dc, 'consumption', null);
+                  }
+
+                  return dc;
+                });
+            }
+
+            return this.$q.when(dc);
+          }));
+    }
+
     loadDatastores({ offset, pageSize }) {
       return this.DedicatedCloud
         .getDatastores(
@@ -43,43 +73,14 @@ angular
           this.$stateParams.datacenterId,
           pageSize, offset - 1,
         )
-        .then((result) => {
-          const datastores = _.get(result, 'list.results');
-
-          return this.$q
-            .all(datastores
-              .map((dc) => {
-                if (dc.billing === this.RESOURCE_BILLING_TYPES.hourly) {
-                  return this.dedicatedCloudDataCenterDatastoreService
-                    .fetchLegacyHourlyConsumption(
-                      this.$stateParams.productId,
-                      this.$stateParams.datacenterId,
-                      dc.id,
-                    )
-                    .then((response) => {
-                      _.set(dc, 'consumption', _.get(response, 'consumption.value'));
-                      _.set(dc, 'consumptionLastUpdate', response.lastUpdate);
-                      return dc;
-                    }).catch((err) => {
-                      if (this.noConsumptionResponse.test(err.message)) {
-                        _.set(dc, 'consumption', 0);
-                      } else {
-                        _.set(dc, 'consumption', null);
-                      }
-
-                      return dc;
-                    });
-                }
-
-                return this.$q.when(dc);
-              }))
-            .then(data => ({
-              data,
-              meta: {
-                totalCount: result.count,
-              },
-            }));
-        });
+        .then(result => this
+          .fetchLegacyConsumption(result.list.results)
+          .then(data => ({
+            data,
+            meta: {
+              totalCount: result.count,
+            },
+          })));
     }
 
     orderDatastore(datacenter) {
