c156eb1f3f648b8f44c1cb1e25a92126941e3e15
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-06-04 16:51:44 +0200

    refactor(pcc.datacenter.drp): move activation at step 2 validation

--- client/app/dedicatedCloud/datacenter/drp/steps/ovh/secondPcc/dedicatedCloud-datacenter-drp-ovh-secondPccStep.controller.js
+++ client/app/dedicatedCloud/datacenter/drp/steps/ovh/secondPcc/dedicatedCloud-datacenter-drp-ovh-secondPccStep.controller.js
@@ -1,8 +1,9 @@
 export default class {
   /* @ngInject */
   constructor(
-    $q, $state, $stateParams, $translate, $uibModal,
-    Alerter, DedicatedCloud, DedicatedCloudDrp, ipFeatureAvailability, OvhApiDedicatedCloud,
+    $q, $state, $stateParams, $timeout, $translate, $uibModal, $window,
+    Alerter, DedicatedCloud, dedicatedCloudDrp, ipFeatureAvailability, OvhApiDedicatedCloud,
+    DEDICATED_CLOUD_CONSTANTS,
     DEDICATEDCLOUD_DATACENTER_DRP_IP_USAGE_MAC_ADDRESS_REG_EXP,
     DEDICATEDCLOUD_DATACENTER_DRP_STATUS,
     DEDICATEDCLOUD_DATACENTER_DRP_UNAVAILABLE_IP_STATUS,
@@ -10,15 +11,18 @@ export default class {
     this.$q = $q;
     this.$state = $state;
     this.$stateParams = $stateParams;
+    this.$timeout = $timeout;
     this.$translate = $translate;
     this.$uibModal = $uibModal;
+    this.$window = $window;
     this.Alerter = Alerter;
     this.DedicatedCloud = DedicatedCloud;
-    this.DedicatedCloudDrp = DedicatedCloudDrp;
+    this.dedicatedCloudDrp = dedicatedCloudDrp;
     this.ipFeatureAvailability = ipFeatureAvailability;
     this.OvhApiDedicatedCloud = OvhApiDedicatedCloud;
-    this.MAC_ADDRESS_REG_EXP = DEDICATEDCLOUD_DATACENTER_DRP_IP_USAGE_MAC_ADDRESS_REG_EXP;
     this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS = DEDICATEDCLOUD_DATACENTER_DRP_STATUS;
+    this.MAC_ADDRESS_REG_EXP = DEDICATEDCLOUD_DATACENTER_DRP_IP_USAGE_MAC_ADDRESS_REG_EXP;
+    this.PCC_NEW_GENERATION = DEDICATED_CLOUD_CONSTANTS.pccNewGeneration;
     this.UNAVAILABLE_IP_STATUSES = DEDICATEDCLOUD_DATACENTER_DRP_UNAVAILABLE_IP_STATUS;
   }
 
@@ -123,13 +127,62 @@ export default class {
     return this.$state.go('app.dedicatedClouds.datacenter.drp.ovh.mainPccStep', { drpInformations: this.drpInformations });
   }
 
-  goToNextStep() {
-    return this.$state.go('app.dedicatedClouds.datacenter.drp.ovh.confirmationStep', { drpInformations: this.drpInformations });
-  }
-
   isStepValid() {
     return this.selectedSecondaryPcc
       && this.drpInformations.secondaryDatacenter
       && this.selectedSecondaryIpAddress;
+  }
+
+  validateConfiguration() {
+    this.isLoading = true;
+
+    return this.dedicatedCloudDrp.enableDrp(
+      this.drpInformations,
+      this.drpInformations.primaryPcc.generation !== this.PCC_NEW_GENERATION,
+    )
+      .then((enableDrp) => {
+        if (enableDrp.url !== undefined) {
+          this.storeZertoOptionOrderInUserPref(this.drpInformations, enableDrp);
+          if (!enableDrp.hasAutoPay) {
+            this.$window.open(enableDrp.url, '_blank');
+          }
+        }
+
+        return this.$state.go('app.dedicatedClouds', { reload: true }).then(() => {
+          // $timeout necessary to display alerter message
+          this.$timeout(() => {
+            this.Alerter.set(
+              'alert-info',
+              `
+              ${this.$translate.instant('dedicatedCloud_datacenter_drp_confirm_creation_pending')} ${this.$translate.instant('dedicatedCloud_datacenter_drp_confirm_creation_pending_task')}
+            `,
+              null,
+              'dedicatedCloud_alert',
+            );
+          }, 0);
+        });
+      })
+      .catch((error) => {
+        this.Alerter.error(`${this.$translate.instant('dedicatedCloud_datacenter_drp_confirm_create_error')} ${_.get(error, 'data.message', error.message)}`, 'dedicatedCloudDatacenterDrpAlert');
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  storeZertoOptionOrderInUserPref(drpInformations, enableDrp) {
+    const drpInformationsToStore = {
+      drpInformations,
+      zertoOptionOrderId: enableDrp.orderId,
+      zertoOption: drpInformations.drpType === this.DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS.ovh
+        ? this.DEDICATEDCLOUD_DATACENTER_DRP_ORDER_OPTIONS.zertoOption.ovh
+        : this.DEDICATEDCLOUD_DATACENTER_DRP_ORDER_OPTIONS.zertoOption.onPremises,
+    };
+
+    const { splitter } = this.DEDICATEDCLOUD_DATACENTER_ZERTO;
+    const [, ...[formattedServiceName]] = drpInformations.primaryPcc.serviceName.split(splitter);
+    const preferenceKey = `${this.DEDICATEDCLOUD_DATACENTER_ZERTO.title}_${formattedServiceName.replace(/-/g, '')}`;
+
+    return this.ovhUserPref.create(preferenceKey, drpInformationsToStore);
   }
 }
