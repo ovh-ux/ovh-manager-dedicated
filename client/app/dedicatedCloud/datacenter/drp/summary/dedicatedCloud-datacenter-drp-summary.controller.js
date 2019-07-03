60f3319727ee7cb4f80d5c20b9d5d00ea80d8772
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-07-03 09:46:33 +0200

    feat(drp): add vpn configuration

--- client/app/dedicatedCloud/datacenter/drp/summary/dedicatedCloud-datacenter-drp-summary.controller.js
+++ client/app/dedicatedCloud/datacenter/drp/summary/dedicatedCloud-datacenter-drp-summary.controller.js
@@ -1,27 +1,37 @@
 export default class {
   /* @ngInject */
   constructor(
-    $state, $stateParams, $timeout, $translate, $uibModal,
-    Alerter, dedicatedCloudDrp, OvhApiMe,
-    DEDICATEDCLOUD_DATACENTER_DRP_STATUS,
+    $state, $timeout, $translate,
+    Alerter, dedicatedCloudDrp, OvhApiMe, Validator,
+    DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS, DEDICATEDCLOUD_DATACENTER_DRP_STATUS,
+    DEDICATEDCLOUD_DATACENTER_DRP_VPN_CONFIGURATION_STATUS,
   ) {
     this.$state = $state;
-    this.$stateParams = $stateParams;
     this.$timeout = $timeout;
     this.$translate = $translate;
     this.$uibModal = $uibModal;
     this.Alerter = Alerter;
     this.dedicatedCloudDrp = dedicatedCloudDrp;
     this.OvhApiMe = OvhApiMe;
+    this.Validator = Validator;
+    this.DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS = DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS;
     this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS = DEDICATEDCLOUD_DATACENTER_DRP_STATUS;
+    this.VPN_CONFIGURATION_STATUS = DEDICATEDCLOUD_DATACENTER_DRP_VPN_CONFIGURATION_STATUS;
   }
 
   $onInit() {
-    this.drpInformations = this.$stateParams.drpInformations;
+    this.drpInformations = this.$state.params.drpInformations;
     this.drpStatus = this.dedicatedCloudDrp.constructor.formatStatus(this.currentDrp.state);
 
     this.email = this.currentUser.email;
-    this.deleteActionAvailable = this.dedicatedCloudDrp.constructor.formatStatus(_.get(this.currentDrp, 'remoteSiteInformation.state')) === this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS.delivered;
+    this.deleteActionAvailable = this.drpInformations
+      .drpType === this.DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS.onPremise
+      ? true
+      : this.dedicatedCloudDrp.constructor.formatStatus(_.get(this.currentDrp, 'remoteSiteInformation.state')) === this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS.delivered;
+
+    this.ipValidator = (() => ({
+      test: ip => this.Validator.isValidIpv4(ip),
+    }))();
   }
 
   regenerateZsspPassword() {
@@ -65,5 +75,36 @@ export default class {
       this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS.toProvision,
       this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS.provisionning,
     ].includes(this.currentDrp.state);
+  }
+
+  isDrpTypeOnPremise() {
+    return this.drpInformations.drpType === this.DEDICATEDCLOUD_DATACENTER_DRP_OPTIONS.onPremise;
+  }
+
+  canSetVpnConfiguration() {
+    return this.isDrpTypeOnPremise()
+      && this.drpInformations.state === this.DEDICATEDCLOUD_DATACENTER_DRP_STATUS.delivered
+      && this.drpInformations.vpnConfiguration.vpnConfigState
+        !== this.VPN_CONFIGURATION_STATUS.configuring;
+  }
+
+  hasNoVpnConfiguration() {
+    return this.drpInformations.vpnConfiguration
+      .vpnConfigState === this.VPN_CONFIGURATION_STATUS.notConfigured;
+  }
+
+  validateVpnConfiguration() {
+    this.isValidatingVpnConfiguration = true;
+    return this.dedicatedCloudDrp.configureVpn(this.drpInformations)
+      .then(() => this.$state.go('app.dedicatedClouds')
+        .then(() => {
+          this.Alerter.success(this.$translate.instant('dedicatedCloud_datacenter_drp_vpn_success', 'dedicatedCloudDatacenterDrpAlert'));
+        }))
+      .catch(() => {
+        this.Alerter.error(this.$translate.instant('dedicatedCloud_datacenter_drp_vpn_error', 'dedicatedCloudDatacenterDrpAlert'));
+      })
+      .finally(() => {
+        this.isValidatingVpnConfiguration = true;
+      });
   }
 }
