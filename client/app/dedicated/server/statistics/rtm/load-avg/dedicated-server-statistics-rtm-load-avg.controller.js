03bca3a243edd8e2868064bc9ef4cfa220bd4a48
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2019-07-04 15:23:59 +0530

    fix(server-rtm): load average graph fix
    prevention of graph data overlap between mrtg and load average graphs
    
    closes MANAGER-2566

--- client/app/dedicated/server/statistics/rtm/load-avg/dedicated-server-statistics-rtm-load-avg.controller.js
+++ client/app/dedicated/server/statistics/rtm/load-avg/dedicated-server-statistics-rtm-load-avg.controller.js
@@ -1,4 +1,4 @@
-angular.module('controllers').controller('controllers.Server.Stats.Loadavg', ($scope, $location, $timeout, $stateParams, Server) => {
+angular.module('controllers').controller('controllers.Server.Stats.Loadavg', ($scope, $stateParams, Server) => {
   function timestampToDate(timestamp) {
     return moment.unix(timestamp / 1000).calendar();
   }
@@ -8,20 +8,20 @@ angular.module('controllers').controller('controllers.Server.Stats.Loadavg', ($s
   }
 
   function buildChart(data) {
-    $scope.series = [];
-    $scope.data = [];
-    $scope.labels = [];
+    $scope.loadAvgSeries = [];
+    $scope.loadAvgData = [];
+    $scope.loadAvgLabels = [];
     angular.forEach(data, (avgData, avgName) => {
       _.each(_.get(avgData, 'points'), (point) => {
-        if ($scope.labels.indexOf(point[0]) === -1) {
-          $scope.labels.push(point[0]);
+        if ($scope.loadAvgLabels.indexOf(point[0]) === -1) {
+          $scope.loadAvgLabels.push(point[0]);
         }
       });
-      $scope.series.push(avgName);
-      $scope.data.push(convertData(_.get(avgData, 'points')));
+      $scope.loadAvgSeries.push(avgName);
+      $scope.loadAvgData.push(convertData(_.get(avgData, 'points')));
     });
-    $scope.labels = _.map(_.sortBy($scope.labels), value => timestampToDate(value));
-    $scope.options = {
+    $scope.loadAvgLabels = _.map(_.sortBy($scope.loadAvgLabels), value => timestampToDate(value));
+    $scope.loadAvgOptions = {
       legend: {
         display: true,
         position: 'top',
@@ -30,13 +30,13 @@ angular.module('controllers').controller('controllers.Server.Stats.Loadavg', ($s
   }
 
   function init() {
-    $scope.loading = true;
+    $scope.loadAvgLoading = true;
     Server.getStatisticsLoadavg($stateParams.productId, {
       period: $scope.selectedPeriod.value,
     }).then((data) => {
       buildChart(data);
     }).finally(() => {
-      $scope.loading = false;
+      $scope.loadAvgLoading = false;
     });
   }
 
