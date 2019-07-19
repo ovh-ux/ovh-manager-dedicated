48d725e97dbeb0398f10d46f025cb357bb038f18
Marie JONES <marie.jones@corp.ovh.com>
2019-07-19 14:39:31 +0200

    refactor(billing.service): turn service to a class and extract constants

--- client/app/billing/autoRenew/billing-autoRenew.service.js
+++ client/app/billing/autoRenew/billing-autoRenew.service.js
@@ -1,31 +1,27 @@
-angular.module('Billing.services').service('BillingAutoRenew', [
-  '$q',
-  'OvhApiEmailExchange',
-  'OvhHttp',
-  function ($q, OvhApiEmailExchange, OvhHttp) {
-    const AUTORENEW_CONTRACT_CA = 1752;
-    const AUTORENEW_CONTRACT_WE = 1754;
-    const AUTORENEW_CONTRACT_WS = 1755;
-    const AUTORENEW_CONTRACT_QC = 1753;
+import {
+  AUTORENEW_EVENT, NIC_URL, SERVICES_TYPE,
+} from './autorenew.constants';
 
-    const nicRenewUrl = '/me/autorenew';
-    const self = this;
-
-    this.getAutorenewContractIds = () => [
-      AUTORENEW_CONTRACT_CA,
-      AUTORENEW_CONTRACT_WE,
-      AUTORENEW_CONTRACT_WS,
-      AUTORENEW_CONTRACT_QC,
-    ];
+export default class {
+  /* @ngInject */
+  constructor(
+    $q,
+    DucUserContractService,
+    OvhApiEmailExchange,
+    OvhHttp,
+  ) {
+    this.$q = $q;
+    this.DucUserContractService = DucUserContractService;
+    this.OvhApiEmailExchange = OvhApiEmailExchange;
+    this.OvhHttp = OvhHttp;
 
     this.events = {
-      AUTORENEW_CHANGED: 'billing.autorenew.changed',
+      AUTORENEW_CHANGES: AUTORENEW_EVENT,
     };
+  }
 
-    /**
-       * Get paginated auto renew list
-       */
-    this.getServices = (count, offset, search, type, renew, renewal, order, nicBilling) => OvhHttp.get('/billing/autorenew/services', {
+  getServices(count, offset, search, type, renew, renewal, order, nicBilling) {
+    return this.OvhHttp.get('/billing/autorenew/services', {
       rootPath: '2api',
       params: {
         count,
@@ -38,67 +34,14 @@ angular.module('Billing.services').service('BillingAutoRenew', [
         nicBilling,
       },
     });
+  }
 
-    /**
-       * Get services types
-       */
-    this.getServicesTypes = () => $q.when([
-      'ALL_DOM',
-      'CAAS_CONTAINERS',
-      'CDN_DEDICATED',
-      'CDN_WEBSITE',
-      'CDN_WEBSTORAGE',
-      'CLUSTER_HADOOP',
-      'DBAAS_QUEUE',
-      'DEDICATED_CEPH',
-      'DEDICATED_NAS',
-      'DEDICATED_NASHA',
-      'DEDICATED_SERVER',
-      'DEDICATED_HOUSING',
-      'DEDICATED_CLOUD',
-      'DESKAAS',
-      'DOMAIN',
-      'EMAIL_DOMAIN',
-      'EMAIL_PRO',
-      'HORIZONVIEW',
-      'HOSTING_PRIVATE_DATABASE',
-      'HOSTING_RESELLER',
-      'HOSTING_WEB',
-      'IP_LOADBALANCING',
-      'IP_LOADBALANCER',
-      'LICENCE_CLOUD_LINUX',
-      'LICENCE_CPANEL',
-      'LICENCE_DIRECT_ADMIN',
-      'LICENCE_PLESK',
-      'LICENCE_VIRTUOZZO',
-      'LICENCE_WINDOWS',
-      'LICENCE_WORKLIGHT',
-      'LICENCE_OFFICE',
-      'LICENCE_SQL_SERVER',
-      'OVER_THE_BOX',
-      'PAAS_DATABASE',
-      'PAAS_MONITORING',
-      'DBAAS_TIMESERIES',
-      'DBAAS_LOGS',
-      'ROUTER',
-      'SAAS_CSP2',
-      'TELEPHONY',
-      'VPS',
-      'XDSL',
-      'EXCHANGE',
-      'MIS',
-      'SSL_GATEWAY',
-      'METRICS',
-      'VEEAM_CLOUD_CONNECT',
-      'VEEAM_ENTERPRISE',
-      'SHAREPOINT',
-      'VIP',
-    ]);
+  getServicesTypes() {
+    return this.$q.when(SERVICES_TYPE);
+  }
 
-    /**
-       * update service list
-       */
-    this.updateServices = updateList => OvhHttp.put('/sws/billing/autorenew/services/update', {
+  updateServices(updateList) {
+    return this.OvhHttp.put('/sws/billing/autorenew/services/update', {
       rootPath: '2api',
       data: {
         updateList,
@@ -108,67 +51,86 @@ angular.module('Billing.services').service('BillingAutoRenew', [
         return result;
       }
       _.set(result, 'state', 'ERROR');
-      return $q.reject(result);
+      return this.$q.reject(result);
     });
+  }
 
-    this.getAutorenew = () => OvhHttp.get(nicRenewUrl, {
+  getAutorenew() {
+    return this.OvhHttp.get(NIC_URL, {
       rootPath: 'apiv6',
     });
+  }
 
-    this.putAutorenew = renewParam => OvhHttp.put(nicRenewUrl, {
+
+  putAutorenew(renewParam) {
+    return this.OvhHttp.put(NIC_URL, {
       rootPath: 'apiv6',
       data: renewParam,
-      broadcast: self.events.AUTORENEW_CHANGED,
+      broadcast: AUTORENEW_EVENT,
     });
+  }
 
-    this.enableAutorenew = renewDay => OvhHttp.post(nicRenewUrl, {
+  enableAutorenew(renewDay) {
+    return this.OvhHttp.post(NIC_URL, {
       rootPath: 'apiv6',
       data: {
         renewDay,
       },
     });
+  }
 
-    this.disableAutoRenewForDomains = () => OvhHttp.post('/me/manualDomainPayment', {
+  disableAutoRenewForDomains() {
+    return this.OvhHttp.post('/me/manualDomainPayment', {
       rootPath: 'apiv6',
     });
+  }
 
-    this.terminateHosting = serviceName => OvhHttp.post('/hosting/web/{hosting}/terminate', {
+  terminateHosting(serviceName) {
+    return this.OvhHttp.post('/hosting/web/{hosting}/terminate', {
       rootPath: 'apiv6',
       urlParams: {
         hosting: serviceName,
       },
     });
+  }
 
-    this.terminateEmail = serviceName => OvhHttp.post('/email/domain/{domain}/terminate', {
+  terminateEmail(serviceName) {
+    return this.OvhHttp.post('/email/domain/{domain}/terminate', {
       rootPath: 'apiv6',
       urlParams: {
         domain: serviceName,
       },
     });
+  }
 
-    this.terminateHostingPrivateDatabase = serviceName => OvhHttp.post('/hosting/privateDatabase/{serviceName}/terminate', {
+  terminateHostingPrivateDatabase(serviceName) {
+    return this.OvhHttp.post('/hosting/privateDatabase/{serviceName}/terminate', {
       rootPath: 'apiv6',
       urlParams: {
         serviceName,
       },
     });
+  }
 
-    this.getEmailInfos = serviceName => OvhHttp.get('/email/domain/{domain}', {
+  getEmailInfos(serviceName) {
+    return this.OvhHttp.get('/email/domain/{domain}', {
       rootPath: 'apiv6',
       urlParams: {
         domain: serviceName,
       },
     });
+  }
 
-    this.getUserCertificates = () => OvhHttp.get('/me/certificates', {
+  getUserCertificates() {
+    return this.OvhHttp.get('/me/certificates', {
       rootPath: 'apiv6',
     });
+  }
 
-    this.getExchangeService = (organization, exchange) => (
-      OvhApiEmailExchange.service().Aapi().get({
-        organization,
-        exchange,
-      }).$promise
-    );
-  },
-]);
+  getExchangeService(organization, exchange) {
+    return this.OvhApiEmailExchange.service().Aapi().get({
+      organization,
+      exchange,
+    }).$promise;
+  }
+}
