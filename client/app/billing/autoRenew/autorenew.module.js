4740ea0ead1250caddda59cb89ecf2e96844fb50
Marie JONES <marie.jones@corp.ovh.com>
2019-07-22 11:17:23 +0200

    refactor(billing.autorenew): use state for cancel resiliation modal

--- client/app/billing/autoRenew/autorenew.module.js
+++ client/app/billing/autoRenew/autorenew.module.js
@@ -1,4 +1,5 @@
 import agreements from './agreements/user-agreements.module';
+import cancelResiliation from './actions/cancel-resiliation/cancel-resiliation.module';
 import ssh from './ssh/ssh.module';
 
 import service from './billing-autoRenew.service';
@@ -10,6 +11,7 @@ const moduleName = 'ovhManagerBillingAutorenew';
 angular.module(moduleName, [
   'ui.router',
   agreements,
+  cancelResiliation,
   ssh,
 ])
   .config(routing)
