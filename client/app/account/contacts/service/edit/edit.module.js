ecf9753ca72b0d04d8374740ec65bad104e36245
Marie JONES <marie.jones@corp.ovh.com>
2019-08-22 13:31:01 +0200

    refactor(account.contacts): use resolve pattern with a datagrid

---
+++ client/app/account/contacts/service/edit/edit.module.js
@@ -0,0 +1,13 @@
+import component from './edit.component';
+import routing from './edit.routing';
+
+const moduleName = 'ovhManagerDedicatedAccountContactsServiceEdit';
+
+angular.module(moduleName, [
+
+])
+  .config(routing)
+  .component('accountContactsServiceEdit', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
