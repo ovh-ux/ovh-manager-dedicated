4cec15d05a4f942890c893963f52002e1a224e8f
Jérôme Martin <jerome.martin@corp.ovh.com>
2019-09-11 11:27:43 +0200

    feat: generate interface list

--- client/app/dedicated/server/interfaces/dedicated-server-interfaces.controller.js
+++ client/app/dedicated/server/interfaces/dedicated-server-interfaces.controller.js
@@ -7,24 +7,54 @@ class DedicatedServerInterfacesCtrl {
   }
 
   $onInit() {
+    this.interfaces = [];
     this.physicalInterfaces = [];
     this.virtualInterfaces = [];
     this.serverName = this.$stateParams.productId;
 
-    this.NicService.query({ serverName: this.serverName }).$promise
+    this.fetchInterfaces();
+  }
+
+  fetchInterfaces() {
+    return this.NicService.query({ serverName: this.serverName }).$promise
       .then(macs => this.$q.all(
         macs.map(mac => this.NicService.get({ serverName: this.serverName, mac }).$promise),
       ))
       .then((nics) => {
         this.physicalInterfaces = nics;
-        return this.$q.all(nics.map(mac => this.VniService.get({
-          serverName: this.serverName,
-          uuid: mac.virtualNetworkInterface,
-        }).$promise));
+        return this.$q.all(nics
+          .filter(nic => !!nic.virtualNetworkInterface)
+          .map(nic => this.VniService.get({
+            serverName: this.serverName,
+            uuid: nic.virtualNetworkInterface,
+          }).$promise));
       })
       .then((vnis) => {
         this.virtualInterfaces = vnis;
+        this.interfaces = this.constructor.mergeInterfaces(
+          this.physicalInterfaces,
+          this.virtualInterfaces,
+        );
       });
+  }
+
+  static mergeInterfaces(physicals, virtuals) {
+    return physicals
+      .filter(physical => !virtuals.some(
+        virtual => virtual.networkInterfaceController.includes(physical.mac),
+      ))
+      .map(physical => ({
+        name: physical.mac,
+        mac: physical.mac,
+        type: physical.linkType,
+        vrack: null,
+      }))
+      .concat(virtuals.map(virtual => ({
+        name: virtual.name,
+        mac: virtual.networkInterfaceController.join(', '),
+        type: virtual.mode,
+        vrack: virtual.vrack || '-',
+      })));
   }
 }
 
