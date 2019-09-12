9dee4a141fbfa951176cf73ad16a48fd42e21671
Axel Peter <axel.peter@live.com>
2019-09-12 18:22:01 +0200

    feat(dedicated.server.interfaces): create layout for step checker

--- client/app/dedicated/server/interfaces/ola-step-checker/ola-step-checker.controller.js
+++ client/app/dedicated/server/interfaces/ola-step-checker/ola-step-checker.controller.js
@@ -1,2 +1,10 @@
 export default class {
+  /* @ngInject */
+  constructor($state) {
+    this.$state = $state;
+  }
+
+  goToOlaConfiguration() {
+    this.$state.go('app.dedicated.server.interfaces.ola');
+  }
 }
