dd00e725311cb37e61f6d83a9274006ce8448cb6
Jérôme Martin <jerome.martin@corp.ovh.com>
2019-09-12 11:25:41 +0200

    fix: handle every interface type

--- client/app/dedicated/server/interfaces/interface.class.js
+++ client/app/dedicated/server/interfaces/interface.class.js
@@ -1,6 +1,10 @@
 import {
-  TYPE_PUBLIC,
-  TYPE_VRACK,
+  PHYSICAL_TYPE_PUBLIC,
+  PHYSICAL_TYPE_PRIVATE,
+  PHYSICAL_TYPE_PRIVATE_LAG,
+  VIRTUAL_TYPE_PUBLIC,
+  VIRTUAL_TYPE_VRACK,
+  VIRTUAL_TYPE_VRACK_AGGREGATION,
 } from './interfaces.constants';
 
 export default class Interface {
@@ -9,11 +13,15 @@ export default class Interface {
   }
 
   isPublic() {
-    return this.type === TYPE_PUBLIC;
+    return this.type === PHYSICAL_TYPE_PUBLIC
+      || this.type === VIRTUAL_TYPE_PUBLIC;
   }
 
   isPrivate() {
-    return this.type === TYPE_VRACK;
+    return this.type === PHYSICAL_TYPE_PRIVATE
+      || this.type === PHYSICAL_TYPE_PRIVATE_LAG
+      || this.type === VIRTUAL_TYPE_VRACK
+      || this.type === VIRTUAL_TYPE_VRACK_AGGREGATION;
   }
 
   hasFailoverIps() {
