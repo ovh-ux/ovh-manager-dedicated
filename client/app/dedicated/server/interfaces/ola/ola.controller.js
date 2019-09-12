06a6f2a1ab8f7a79df4ef1c4672e3be929872ece
Axel Peter <axel.peter@live.com>
2019-09-12 11:21:12 +0200

    fix(dedicated.server.interfaces): fix when vni is undefined

--- client/app/dedicated/server/interfaces/ola/ola.controller.js
+++ client/app/dedicated/server/interfaces/ola/ola.controller.js
@@ -8,36 +8,6 @@ export default class {
       mode: this.olaModes[0],
     };
 
-    this.interfaces = [{
-      name: '7C:32:04:48:c2:02',
-      mode: 'public',
-      offer: 'ultimate',
-      bandwidth: {
-        incoming: {
-          unit: 'Mbps',
-          value: 10000,
-        },
-        outgoing: {
-          unit: 'Mbps',
-          value: 2000,
-        },
-      },
-    }, {
-      name: '7C:32:04:48:c2:03',
-      mode: 'private',
-      offer: 'included',
-      bandwidth: {
-        incoming: {
-          unit: 'Mbps',
-          value: 3000,
-        },
-        outgoing: {
-          unit: 'Mbps',
-          value: 3000,
-        },
-      },
-    }];
-
     this.selectedInterfaces = [];
   }
 
