46d7b1f55eb35aa4f22f7376fadba5bf6fb5befd
Axel Peter <axel.peter@live.com>
2019-09-13 18:20:33 +0200

    feat(dedicated.server.interfaces): apply style on step checker

--- client/app/dedicated/server/interfaces/interfaces.routing.js
+++ client/app/dedicated/server/interfaces/interfaces.routing.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dedicated.server.interfaces', {
-    url: '/interfaces',
+    url: '/interfaces?showSteps&currentStep',
     views: {
       'tabView@app.dedicated.server': {
         component: 'dedicatedServerInterfaces',
@@ -12,6 +14,17 @@ export default /* @ngInject */ ($stateProvider) => {
         serverName,
         DedicatedServerInterfacesService,
       ) => DedicatedServerInterfacesService.getInterfaces(serverName),
+
+      bandwidth: /* @ngInject */ (
+        serverName,
+        Server,
+      ) => Server.getBandwidth(serverName),
+
+      olaInfos: /* @ngInject */ DedicatedServerInterfacesService => DedicatedServerInterfacesService.getOlaInfos(), // eslint-disable-line
+      currentStep: /* @ngInject */ $stateParams => (!_.isUndefined($stateParams.currentStep)
+        ? parseInt($stateParams.currentStep, 10)
+        : 0),
+      showSteps: /* @ngInject */ $stateParams => $stateParams.showSteps,
     },
   });
 };
--- client/app/dedicated/server/interfaces/ola-step-checker/ola-step-checker.component.js
+++ client/app/dedicated/server/interfaces/ola-step-checker/ola-step-checker.component.js
@@ -5,6 +5,7 @@ export default {
   controller,
   template,
   bindings: {
-    ola: '<',
+    showSteps: '<',
+    currentStep: '<',
   },
 };
