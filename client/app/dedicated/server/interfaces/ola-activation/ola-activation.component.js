b9f8ffd577372d4e88d2062243b26c6d2c231d0b
Jérôme Martin <jerome.martin@corp.ovh.com>
2019-09-18 16:24:27 +0200

    feat: first attempt at ordering the option

--- client/app/dedicated/server/interfaces/ola-activation/ola-activation.component.js
+++ client/app/dedicated/server/interfaces/ola-activation/ola-activation.component.js
@@ -4,6 +4,8 @@ import template from './ola-activation.html';
 export default {
   bindings: {
     goBack: '<',
+    serviceName: '<',
+    user: '<',
   },
   controller,
   template,
--- client/app/dedicated/server/interfaces/ola-activation/ola-activation.controller.js
+++ client/app/dedicated/server/interfaces/ola-activation/ola-activation.controller.js
@@ -1,8 +1,19 @@
 export default class DedicatedServerInterfacesOlaActivationCtrl {
+  constructor(OvhApiOrderCart) {
+    this.CartService = OvhApiOrderCart;
+  }
+
   activate() {
-    // TODO: API call for OLA activation
-    this.goBack({
-      isOlaActivated: true, // For mockup
-    });
+    this.CartService.v6().post({
+      ovhSubsidiary: this.user.ovhSubsidiary,
+    }).then(cart => this.CartService.ServiceOption().v6().post({
+      productName: 'dedicated',
+      serviceName: this.serviceName,
+      planCode: 'ovh-link-aggregation-infra',
+      duration: 'P1M',
+      pricingMode: 'default',
+      quantity: 1,
+      cartId: cart.cartId,
+    }));
   }
 }
