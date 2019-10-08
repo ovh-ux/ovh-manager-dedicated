adcf2720bdb8a009e672e2e84d286b316111d5bf
Marie JONES <marie.jones@corp.ovh.com>
2019-10-08 11:42:10 +0200

    fix(account.contacts): handle contact update

--- client/app/account/contacts/update/user.contacts-update.routes.js
+++ client/app/account/contacts/update/user.contacts-update.routes.js
@@ -1,25 +1,42 @@
-angular.module('UserAccount').config(($stateProvider) => {
-  $stateProvider.state('app.account.useraccount.contact-update-domain', {
+angular.module('UserAccount').config(($stateProvider, $urlRouterProvider) => {
+  $stateProvider.state('app.account.contact-update-domain', {
     url: '/contact/:currentDomain/:contactId?fields',
     templateUrl: 'account/contacts/update/user-contacts-update.html',
     controller: 'UserAccount.controllers.update',
     controllerAs: 'contactCtrl',
+    translations: {
+      format: 'json',
+      value: ['../../user'],
+    },
   });
 
 
   // TODO: Need refactoring this two steps ... Quickfix to manage legacy url correctly
 
-  $stateProvider.state('app.account.useraccount.contact-update', {
+  $stateProvider.state('app.account.contact-update', {
     url: '/contact/:contactId/?fields',
     templateUrl: 'account/contacts/update/user-contacts-update.html',
     controller: 'UserAccount.controllers.update',
     controllerAs: 'contactCtrl',
+    translations: {
+      format: 'json',
+      value: ['../../user'],
+    },
   });
 
-  $stateProvider.state('app.account.useraccount.contact-update2', {
+  $stateProvider.state('app.account.contact-update2', {
     url: '/contact/:contactId?fields',
     templateUrl: 'account/contacts/update/user-contacts-update.html',
     controller: 'UserAccount.controllers.update',
     controllerAs: 'contactCtrl',
+    translations: {
+      format: 'json',
+      value: ['../../user'],
+    },
   });
+
+  $urlRouterProvider.when(
+    /^\/useraccount\/contact/,
+    $location => $location.url($location.url().replace('/useraccount', '')),
+  );
 });
