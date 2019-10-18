4d67f96d851b7b40b8e1af479ad56cfb909f3193
cyril.biencourt <cyril.biencourt@corp.ovh.com>
2019-10-18 16:17:56 +0200

    feat(mfa): add mfa enrollment page

--- client/app/account/user/security/2fa/user-security-2fa.routing.js
+++ client/app/account/user/security/2fa/user-security-2fa.routing.js
@@ -1,9 +1,7 @@
 angular
   .module('UserAccount')
   .config(/* @ngInject */($stateProvider) => {
-    const name = 'app.account.user.security.mfa';
-
-    $stateProvider.state(name, {
+    $stateProvider.state('app.account.user.security.mfa', {
       url: '/mfa',
       templateUrl: 'account/user/security/2fa/user-security-2fa.html',
       controller: 'UserAccount.controllers.doubleAuth.2fa.enable',
